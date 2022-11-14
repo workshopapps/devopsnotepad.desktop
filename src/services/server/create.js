95766ef9e8afa4ed2949003ae631869d003b3ee0
Chima <orlandofays3@gmail.com>
2022-11-14 17:44:01 +0100

    test precommit for js files

--- src/services/server/create.js
+++ src/services/server/create.js
@@ -1,21 +1,20 @@
 import ServerRepo from "../../database/repositories/ServerRepo.js";
-import {ServiceError} from "../../lib/errors/index.js";
-import {check_ip_status} from "../../utils/index.js";
+import { ServiceError } from "../../lib/errors/index.js";
+import { check_ip_status } from "../../utils/index.js";
 
-
-export default async function create (params) {
-    const {name, ipAddress} = params;
+export default async function create(params) {
+    const { name, ipAddress } = params;
 
     const existingServer = await ServerRepo.getServerByName(name);
-    if (existingServer) throw new ServiceError('Server already exists');
+    if (existingServer) throw new ServiceError("Server already exists");
 
     await ServerRepo.create(params);
     const server = await ServerRepo.getServerByName(name);
 
-    const online = await check_ip_status(ipAddress);
+    const isOnline = await check_ip_status(ipAddress);
 
     return {
         server,
-        online
+        isOnline,
     };
 }
