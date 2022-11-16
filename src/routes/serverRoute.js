98b41830c15ca1fc4612d008a70817c26127951d
Mas'ud Ndatsu <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-11-16 22:40:11 +0100

    delete test added

--- src/routes/serverRoute.js
+++ src/routes/serverRoute.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post("/", ServerController.create);
 router.patch("/", ServerController.update);
+router.post("/remove_all", ServerController.deleteServersById);
+router.delete("/:id", ServerController.deleteServerById);
 
 export default router;
--- test/integration/Server.test.js
+++ test/integration/Server.test.js
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import supertest from "supertest";
 import app from "../../src/initialize.js";
 import Documentator from "../../src/utils/documentator/index.js";
@@ -12,50 +12,71 @@ let userId;
 
 //Deletes every record from servers table before any test is run to avoid collisions.
 before(async () => {
-    await connection.raw("delete from servers");
+  await connection.raw("delete from servers");
 });
 
 describe("Server", () => {
-    it("should create new server", async () => {
-        const res = await request.post("/server").send({
-            name: "example server",
-            ipAddress: "google.com",
-        });
-
-        userId = res.body.server.id;
-
-        assert.equal(res.status, 200);
-        docmaker.addEndpoint(res);
+  it("should create new server", async () => {
+    const res = await request.post("/server").send({
+      name: "example server",
+      ipAddress: "google.com",
     });
 
-    it("should throw error when creating a server with an existing name", async () => {
-        const res = await request.post("/server").send({
-            name: "example server",
-            ipAddress: "google.com",
-        });
+    userId = res.body.server.id;
 
-        assert.equal(res.status, 400);
-        assert.include(res.body.message, "Server already exists");
+    assert.equal(res.status, 200);
+    docmaker.addEndpoint(res);
+  });
+
+  it("should throw error when creating a server with an existing name", async () => {
+    const res = await request.post("/server").send({
+      name: "example server",
+      ipAddress: "google.com",
     });
 
-    it("should update server", async () => {
-        const res = await request.patch("/server").send({
-            id: userId,
-            name: "updated server name",
-        });
+    assert.equal(res.status, 400);
+    assert.include(res.body.message, "Server already exists");
+  });
 
-        assert.equal(res.status, 200);
-        assert.include(res.body.message, "Server updated successfully");
-        docmaker.addEndpoint(res);
+  it("should update server", async () => {
+    const res = await request.patch("/server").send({
+      id: userId,
+      name: "updated server name",
     });
 
-    it("should not update server if server is not found", async () => {
-        const res = await request.patch("/server").send({
-            id: "33344knkn323j5kln23l4n3l4n",
-            name: "updated server name",
-        });
+    assert.equal(res.status, 200);
+    assert.include(res.body.message, "Server updated successfully");
+    docmaker.addEndpoint(res);
+  });
 
-        assert.equal(res.status, 404);
-        assert.include(res.body.message, "Server does not exist");
+  it("should not update server if server is not found", async () => {
+    const res = await request.patch("/server").send({
+      id: "33344knkn323j5kln23l4n3l4n",
+      name: "updated server name",
     });
+
+    assert.equal(res.status, 404);
+    assert.include(res.body.message, "Server does not exist");
+  });
+  it("Should delete an existing server", async () => {
+    const res = await request.delete("/server/" + userId).send();
+
+    expect(res.status).to.equal(200);
+    expect(res.body.message).to.include("server deleted successfully");
+    docmaker.addEndpoint(res);
+  });
+  it("Should delete an array existing servers", async () => {
+    const data = await request.post("/server").send({
+      name: "Apache server",
+      ipAddress: "apache.com",
+    });
+    let new_id = data.body.server.id;
+    const res = await request.post("/server/remove_all").send({
+      serverIds: [new_id],
+    });
+
+    expect(res.status).to.equal(200);
+    expect(res.body.message).to.include("servers deleted successfully");
+    docmaker.addEndpoint(res);
+  });
 });
