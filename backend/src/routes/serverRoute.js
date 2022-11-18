2ffeb6975461f9638c988f09dfe740bba146a20d
Mas'ud Ndatsu <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-11-18 19:20:25 +0100

    feat(BE-3): added endpoint for  deleting servers

--- backend/src/routes/serverRoute.js
+++ backend/src/routes/serverRoute.js
@@ -1,9 +1,10 @@
-import express from "express";
-import ServerController from "../controllers/ServerController.js";
+import express from 'express';
+import ServerController from '../controllers/ServerController.js';
 
 const router = express.Router();
 
-router.post("/", ServerController.create);
-router.patch("/", ServerController.update);
+router.post('/', ServerController.create);
+router.patch('/', ServerController.update);
+router.delete('/', ServerController.deleteServersById);
 
 export default router;
