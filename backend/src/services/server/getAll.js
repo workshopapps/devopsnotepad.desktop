c8d9d15c7383f7505126ae7907913ab2be7e8d3f
Mofe <moflux620@gmail.com>
2022-11-18 20:39:00 +0100

    feat(BE-2): added endpoints to get servers that have been added on the app, alongside tests for those endpoints

---
+++ backend/src/services/server/getAll.js
@@ -0,0 +1,14 @@
+import ServerRepo from "../../database/repositories/ServerRepo.js";
+import { NotFoundError } from "../../lib/errors/index.js";
+
+export default async function getAllServers(query) {
+    const { device } = query;
+
+    const servers = await ServerRepo.getServersByDevice(device);
+    if (!servers.length > 0) throw new NotFoundError("There are no servers added on this device");
+
+    return {
+        message: `${servers.length} servers found on this device`,
+        servers,
+    };
+}
