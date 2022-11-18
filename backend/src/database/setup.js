1ebe2e73cb8b40358d788467f1e287e15a790de5
Usenmfon <usenmfonuko@gmail.com>
2022-11-18 20:42:50 +0100

    feat(BE-4): added endpoint to save new notification to db

--- backend/src/database/setup.js
+++ backend/src/database/setup.js
@@ -1,12 +1,12 @@
-import knex from 'knex';
-import {development} from '../../knexfile.js';
+import knex from "knex";
+import {development} from "../../knexfile.js";
 
 
 const connection = knex(development);
 
 export const migrate = async () => {
-  await connection.migrate.latest();
-  console.log('Migration complete');
+    await connection.migrate.latest();
+    console.log("Migration complete");
 };
 
 export default connection;
