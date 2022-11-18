b4dd07a62c3d98a9860a545967d974efdfbcff84
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-19 00:16:13 +0100

    feat(BE-5) implemented weekly and monthly notifications

--- backend/src/services/notification/get.js
+++ backend/src/services/notification/get.js
@@ -2,7 +2,18 @@ import NotificationRepo from "../../database/repositories/notificationRepo.js";
 import { NotFoundError } from "../../lib/errors/index.js";
 
 export default async function get(params, query) {
-    const notifications = await NotificationRepo.getNotification(params, query);
+    let notifications = [];
+    switch (query.range) {
+    case "weekly":
+        notifications = await NotificationRepo.getWeeklyNotifications(params, query);
+        break;
+    case "monthly":
+        notifications = await NotificationRepo.getMonthlyNotifications(params, query);
+        break;
+    default:
+        notifications = await NotificationRepo.getNotifications(params, query);
+        break;
+    }
     if (!notifications) throw new NotFoundError("An error occured while fetching notifications");
     return {
         notifications,
