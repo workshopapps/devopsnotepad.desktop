b0f74b1ee2abc5f04dd81494a0756cb8bfa9bfbd
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-18 17:26:44 +0100

    feat(BE-5): added notifications route to bind to server route

--- backend/src/routes/serverRoute.js
+++ backend/src/routes/serverRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
 import ServerController from "../controllers/ServerController.js";
+import NotificationController from "../controllers/notificationController.js";
 
 const router = express.Router();
+
+router.get("/:serverId/notifications", NotificationController.get);
 
 router.post("/", ServerController.create);
 router.patch("/", ServerController.update);
