36d5dbc48e45a31303bcbc445ad0146a70389742
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-19 00:34:54 +0100

    feat: dependencies installed

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 import Header from "./Component/Header/Header";
+import Csr from "./Pages/Csr/Csr";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <Routes>
-        {/* <Route path="/" exact element={<Integration />} /> */}
+        <Route path="/" exact element={<Csr />} />
       </Routes>
       <Footer />
     </React.Fragment>
