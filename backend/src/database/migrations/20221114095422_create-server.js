81f623e4f8cd26718dccb882a35233d65caf5bda
Mofe <moflux620@gmail.com>
2022-11-18 20:39:00 +0100

    feat(BE-2): added endpoints to get servers that have been added on the app, alongside tests for those endpoints

--- backend/src/database/migrations/20221114095422_create-server.js
+++ backend/src/database/migrations/20221114095422_create-server.js
@@ -1,18 +1,14 @@
-
 export async function up(knex) {
-    return knex.schema.createTable('servers', table => {
-        table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
-        table.string('name', 255).notNullable();
-        table.string('ipAddress', 255).notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table
-            .dateTime('updated_at')
-            .notNullable()
-            .defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-    });
+  return knex.schema.createTable('servers', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
+    table.string('name', 255).notNullable();
+    table.string('device_id', 255).notNullable();
+    table.string('ipAddress', 255).notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+  });
 }
 
-
 export function down(knex) {
-    return knex.schema.dropTable('servers');
+  return knex.schema.dropTable('servers');
 }
