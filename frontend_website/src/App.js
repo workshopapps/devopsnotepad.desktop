2bd65b1e9c3ca150ef52bf5be0f4068c4bdcd6d4
Apatira Temitope <83637849+Temitope-Apatira@users.noreply.github.com>
2022-11-18 23:41:32 +0100

    feat: dependencies

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary } from 'react-error-boundary';
 
-
+import { About } from './Pages/About/About';
 import Footer from './Component/Footer/Footer';
 import Navigation from './Component/Navigation/Navigation';
 import Faq from './Pages/Faq/Faq';
 import ComingSoon from './Pages/ComingSoon/ComingSoon';
 import CareerPage from './Pages/CareerPage/CareerPage';
 
-import classes from './App.module.css'
+import classes from './App.module.css';
 
 // Error Boundary FallbackComponent: This is the function that will be called whenever the errorboundary component caught an error
 const ErrorFallback = (props) => {
   return (
-    <div role="alert" className={classes.error}>
+    <div role='alert' className={classes.error}>
       <p className={classes.p}>Something went wrong!</p>
       <pre className={classes.pre}>{props.error.message}</pre>
-      <button onClick={props.resetErrorBoundary} className={classes.button}>Restart app</button>
+      <button onClick={props.resetErrorBoundary} className={classes.button}>
+        Restart app
+      </button>
     </div>
   );
 };
 
 function App() {
-
   const navigate = useNavigate();
 
   return (
@@ -34,19 +35,20 @@ function App() {
       <ErrorBoundary
         FallbackComponent={ErrorFallback}
         onReset={() => {
-          navigate("/");
+          navigate('/');
         }}
       >
         <Routes>
           {/* <Route path="/" exact element={<Integration />} /> */}
-          <Route path="/faq" exact element={<Faq />} />
-          <Route path="/comingsoon" exact element={<ComingSoon />} />
-          <Route path="/career" exact element={<CareerPage />} />
+          <Route path='/faq' exact element={<Faq />} />
+          <Route path='/comingsoon' exact element={<ComingSoon />} />
+          <Route path='/career' exact element={<CareerPage />} />
+          <Route path='/about' exact element={<About />} />
         </Routes>
       </ErrorBoundary>
 
       <Footer />
-    </React.Fragment >
+    </React.Fragment>
   );
 }
 
