5ea532895933a3e6ebab9c3601ad5c4c643fcedb
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-18 22:14:12 +0100

    initial commit prices page: installed needed label and created components as well as a data file

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './Component/Footer/Footer';
 import Navigation from './Component/Navigation/Navigation';
 import Faq from './Pages/Faq/Faq';
+import Prices from './Pages/Prices/Prices';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         {/* <Route path="/" exact element={<Integration />} /> */}
         <Route path="/faq" exact element={<Faq />} />
+        <Route path="/prices" element={<Prices />} />
       </Routes>
       <Footer />
     </React.Fragment>
---
+++ frontend_website/src/Pages/Prices/Data.js
@@ -0,0 +1,77 @@
+const Data = [
+  {
+    items: [
+      {
+        title: 'Starter',
+        price: 'Free',
+        list: ['Three servers', 'Unlimited notes', 'Three downloads'],
+        button: 'Choose Starter',
+        class: 'transparent',
+      },
+      {
+        title: 'Professional',
+        price: '#5,000 / month',
+        list: [
+          'Unlimited servers',
+          'Unlimited notes',
+          'Unlimited downloads',
+          'Third party integration',
+        ],
+        button: 'Choose Professional',
+        class: 'colored',
+      },
+      {
+        title: 'Organization',
+        price: '#10,000 / month',
+        list: [
+          'Unlimited servers',
+          'Unlimited notes',
+          'Unlimited downloads',
+          'Third party integration',
+          'Unlimited collaborators',
+        ],
+        button: 'Choose Organization',
+        color: 'transparent',
+      },
+    ],
+  },
+
+  {
+    items: [
+      {
+        title: 'Starter',
+        price: 'Free',
+        list: ['Three servers', 'Unlimited notes', 'Three downloads'],
+        button: 'Choose Starter',
+        class: 'transparent',
+      },
+      {
+        title: 'Professional',
+        price: '#55,000 / year',
+        list: [
+          'Unlimited servers',
+          'Unlimited notes',
+          'Unlimited downloads',
+          'Third party integration',
+        ],
+        button: 'Choose Professional',
+        class: 'colored',
+      },
+      {
+        title: 'Organization',
+        price: '#110,000 / year',
+        list: [
+          'Unlimited servers',
+          'Unlimited notes',
+          'Unlimited downloads',
+          'Third party integration',
+          'Unlimited collaborators',
+        ],
+        button: 'Choose Organization',
+        color: 'transparent',
+      },
+    ],
+  },
+];
+
+export default Data;
