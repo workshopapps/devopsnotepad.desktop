5e4f97822aed152529a6beadfdf8908e138206b4
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-19 23:22:07 +0100

    chore(BE-5): code refactoring

--- backend/src/database/repositories/NotificationRepo.js
+++ backend/src/database/repositories/NotificationRepo.js
@@ -3,28 +3,29 @@ import APIFeatures from "../../utils/apiFeatures.js";
 
 export default class NotificationRepo {
     static getNotifications = async (id, query) => {
-        return new APIFeatures(connection("notifications")
-            .where("serverId", id), query)
+        const dbQuery = connection("notifications")
+            .where("serverId", id);
+        return new APIFeatures(dbQuery, query)
             .paginate()
             .sort()
             .query; 
     };
 
     static getWeeklyNotifications = async (id, query) => {
-        return new APIFeatures(connection("notifications")
+        const dbQuery = connection("notifications")
             .where("serverId", id)
-            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 WEEK AND NOW()`)),
-        query)
+            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 WEEK AND NOW()`));
+        return new APIFeatures(dbQuery, query)
             .paginate()
             .sort()
             .query;
     };
 
     static getMonthlyNotifications = async (id, query) => {
-        return new APIFeatures(connection("notifications")
+        const dbQuery = connection("notifications")
             .where("serverId", id)
-            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 MONTH AND NOW()`)),
-        query)
+            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 MONTH AND NOW()`));
+        return new APIFeatures(dbQuery, query)
             .paginate()
             .sort()
             .query;
