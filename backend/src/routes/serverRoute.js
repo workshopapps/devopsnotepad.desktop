0e650ab0cc7ca4dee434579d08596950900cb192
Mofe <moflux620@gmail.com>
2022-11-19 16:41:03 +0100

    feat(BE-2): added endpoints to get servers that have been added on the app, alongside tests for those endpoints

--- backend/src/routes/serverRoute.js
+++ backend/src/routes/serverRoute.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post("/", ServerController.create);
 router.get("/", ServerController.getAllServers);
-router.get("/:serverId", ServerController.readOne);
 router.patch("/", ServerController.update);
 
 export default router;
