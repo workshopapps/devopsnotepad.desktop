9711b73edd0949e29921a944ef1d4a08cd048778
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-19 10:21:44 +0100

    completed the build of the prices page

--- frontend_website/src/Pages/Prices/Prices.js
+++ frontend_website/src/Pages/Prices/Prices.js
@@ -3,9 +3,16 @@ import './Prices.css';
 import ToggleSwitch from './ToggleSwitch/ToggleSwitch';
 import Data from './Data';
 import PriceCard from './PriceCard';
+import bad from './x.svg';
+import good from './good.svg';
 
 const Prices = () => {
   const [toggleMonthly, setToggleMonthly] = useState(true);
+
+  const handleToggle = (event) => {
+    setToggleMonthly(event.target.checked);
+  };
+
   return (
     <section className="prices-container">
       <div className="prices-header-container">
@@ -15,17 +22,63 @@ const Prices = () => {
         </h1>
         <h3 className="prices-subheader">Choose Your Plan</h3>
         <div className="toggle-component">
-          <ToggleSwitch />
+          <ToggleSwitch
+            toggleMonthly={toggleMonthly}
+            handleToggle={handleToggle}
+          />
         </div>
       </div>
 
-      {toggleMonthly && (
-        <div className="prices-sample-grid">
-          {Data[0].items.map((item, index) => {
-            <PriceCard card={item} id={index} />;
-          })}
+      <div className="prices-sample-grid">
+        {!toggleMonthly
+          ? Data[0].items.map((item, index) => {
+              return <PriceCard card={item} id={index} />;
+            })
+          : Data[1].items.map((card, index) => {
+              return <PriceCard card={card} id={index} />;
+            })}
+      </div>
+
+      <div className="compare-price">
+        <h1>
+          Compare <span className="prices-header-blue">Our Plans</span>
+        </h1>
+        <div className="table-container">
+          {/* <div className="flex-header"> */}
+          <ul>
+            <li>Starter</li>
+            <li>Professional</li>
+            <li>Organization</li>
+          </ul>
+          {/* </div> */}
+          <div className="grid-table">
+            <h9 className="grid-header">Unlimited Number of servers</h9>
+            <img className="grid-item" src={bad} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <h9 className="grid-header">Notes</h9>
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <h9 className="grid-header">Third party integration</h9>
+            <img className="grid-item" src={bad} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <h9 className="grid-header">Collaborators</h9>
+            <img className="grid-item" src={bad} alt="" />
+            <img className="grid-item" src={bad} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <h9 className="grid-header">Unlimited downloads</h9>
+            <img className="grid-item" src={bad} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <h9 className="grid-header">Access to Opspad communinity</h9>
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+            <img className="grid-item" src={good} alt="" />
+          </div>
         </div>
-      )}
+      </div>
     </section>
   );
 };
