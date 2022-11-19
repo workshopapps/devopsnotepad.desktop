870d266073a97fbf06b4992965c0d5ba691b65e4
kevinmbuicode <kevinmushan@gmail.com>
2022-11-19 05:56:30 +0300

    chore:terms & privacy policy

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 import Header from "./Component/Header/Header";
-
+import Terms from "./Pages/TermsOfUSe/Terms";
+import {
+      
+      Routes,
+      Route,
+      Link,
+      BrowserRouter
+  } from 'react-router-dom';
+  import PrivacyPolicy from "./Pages/TermsOfUSe/privacyPolicy";
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <Routes>
-        {/* <Route path="/" exact element={<Integration />} /> */}
-      </Routes>
+     
+           <Routes>
+                 <Route exact path='/terms' element={< Terms />}></Route>
+                 <Route exact path='/privacy' element={<PrivacyPolicy/>}></Route>
+                
+          </Routes>
+
       <Footer />
     </React.Fragment>
+    
+    
   );
 }
 
