a9216b6a4b2371c84fd5fe00c68607e7a9a26b44
Apatira Temitope <83637849+Temitope-Apatira@users.noreply.github.com>
2022-11-19 14:26:23 +0100

    feat: navbar merge errors

--- frontend_website/src/Component/Navigation/Navbar.js
+++ frontend_website/src/Component/Navigation/Navbar.js
@@ -1,32 +1,35 @@
 import { useState } from 'react';
 import { menuList } from './NavData';
 import { IoClose } from 'react-icons/io5';
-import logo from "./assets/logo.svg";
+import logo from './assets/logo.svg';
 
 const Navbar = ({ isOpen, setOpen }) => {
   const [menuOpen, setMenuOpen] = useState({});
 
-  const handleMenuToggle = (index) => setMenuOpen(state => ({ [index]: !state[index] }));
+  const handleMenuToggle = (index) =>
+    setMenuOpen((state) => ({ [index]: !state[index] }));
 
   return (
     <nav className={`inMenuBar ${isOpen ? 'openMenu' : ''}`}>
-      <div className="logo">
-        <div className="inLogo">
-          <img src={logo} alt="" />
+      <div className='logo'>
+        <div className='inLogo'>
+          <img src={logo} alt='' />
           <IoClose className='closeIcon' onClick={() => setOpen(false)} />
         </div>
       </div>
-      <ul className="menuUlList">
+      <ul className='menuUlList'>
         {/* <li><a href="/">Home</a></li> */}
-        <div className="menuBox">
-          <button className="menu_title"
+        <div className='menuBox'>
+          <button
+            className='menu_title'
             onClick={() => {
-              setProductDropdown(!productDropdown)
-              setCompanyDropdown(false)
-              setResourcesDropdown(false)
-              setCommunityDropdown(false)
-            }}>
-            Product <i className="fa-solid fa-angle-down"></i>
+              setProductDropdown(!productDropdown);
+              setCompanyDropdown(false);
+              setResourcesDropdown(false);
+              setCommunityDropdown(false);
+            }}
+          >
+            Product <i className='fa-solid fa-angle-down'></i>
           </button>
           <div className={`dropdown ${productDropdown ? 'show' : ''}`}>
             {menuList.map((menu, i) => (
@@ -45,15 +48,17 @@ const Navbar = ({ isOpen, setOpen }) => {
           </div>
         </div>
 
-        <div className="menuBox">
-          <button className="menu_title"
+        <div className='menuBox'>
+          <button
+            className='menu_title'
             onClick={() => {
-              setCompanyDropdown(!companyDropdown)
-              setProductDropdown(false)
-              setResourcesDropdown(false)
-              setCommunityDropdown(false)
-            }}>
-            Company <i className="fa-solid fa-angle-down"></i>
+              setCompanyDropdown(!companyDropdown);
+              setProductDropdown(false);
+              setResourcesDropdown(false);
+              setCommunityDropdown(false);
+            }}
+          >
+            Company <i className='fa-solid fa-angle-down'></i>
           </button>
           <div className={`dropdown ${companyDropdown ? 'show' : ''}`}>
             {menuList.map((menu, i) => (
@@ -71,15 +76,17 @@ const Navbar = ({ isOpen, setOpen }) => {
             ))}
           </div>
         </div>
-        <div className="menuBox">
-          <button className="menu_title"
+        <div className='menuBox'>
+          <button
+            className='menu_title'
             onClick={() => {
-              setResourcesDropdown(!resourcesDropdown)
-              setProductDropdown(false)
-              setCompanyDropdown(false)
-              setCommunityDropdown(false)
-            }}>
-            Resources <i className="fa-solid fa-angle-down"></i>
+              setResourcesDropdown(!resourcesDropdown);
+              setProductDropdown(false);
+              setCompanyDropdown(false);
+              setCommunityDropdown(false);
+            }}
+          >
+            Resources <i className='fa-solid fa-angle-down'></i>
           </button>
           <div className={`dropdown ${resourcesDropdown ? 'show' : ''}`}>
             {menuList.map((menu, i) => (
@@ -95,39 +102,45 @@ const Navbar = ({ isOpen, setOpen }) => {
                 )}
               </div>
             ))}
-        </div>
+          </div>
 
-        <div className="menuBox">
-          <button className="menu_title"
-            onClick={() => {
-              setCommunityDropdown(!communityDropdown)
-              setProductDropdown(false)
-              setCompanyDropdown(false)
-              setResourcesDropdown(false)
-            }}>
-            Community <i className="fa-solid fa-angle-down"></i>
-          </button>
-          <div className={`dropdown ${communityDropdown ? 'show' : ''}`}>
-            {menuList.map((menu, i) => (
-              <div key={i}>
-                {menu.community && (
-                  <li>
-                    {menu.community.map((b, i) => (
-                      <a href={`${b.slug}`} key={i}>
-                        {b.title}
-                      </a>
-                    ))}
-                  </li>
-                )}
-              </div>
-            ))}
+          <div className='menuBox'>
+            <button
+              className='menu_title'
+              onClick={() => {
+                setCommunityDropdown(!communityDropdown);
+                setProductDropdown(false);
+                setCompanyDropdown(false);
+                setResourcesDropdown(false);
+              }}
+            >
+              Community <i className='fa-solid fa-angle-down'></i>
+            </button>
+            <div className={`dropdown ${communityDropdown ? 'show' : ''}`}>
+              {menuList.map((menu, i) => (
+                <div key={i}>
+                  {menu.community && (
+                    <li>
+                      {menu.community.map((b, i) => (
+                        <a href={`${b.slug}`} key={i}>
+                          {b.title}
+                        </a>
+                      ))}
+                    </li>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='navAuthBtn'>
+            <a className='login_link' href='/login'>
+              Login
+            </a>
+            <a className='download_link' href='/'>
+              Download App
+            </a>
           </div>
         </div>
-        <div className="navAuthBtn">
-          <a className="login_link" href="/login">Login</a>
-          <a className="download_link" href="/">Download App</a>
-        </div>
-        
       </ul>
     </nav>
   );
