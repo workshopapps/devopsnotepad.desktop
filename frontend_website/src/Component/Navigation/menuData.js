2207bf9ef49260cc9db5a1953e0dd2a67f155d14
JOEX <oyinjoe23@gmail.com>
2022-11-19 17:45:13 +0100

    desktop page(website)

--- frontend_website/src/Component/Navigation/menuData.js
+++ frontend_website/src/Component/Navigation/menuData.js
@@ -1,18 +1,93 @@
-export const MenuList = [
+export const product = [
   {
-    label: 'Product',
-    slug: '',
+    title: 'Features',
+    slug: '/features',
   },
   {
-    label: 'Company',
-    slug: '',
+    title: 'Prices',
+    slug: '/prices',
   },
   {
-    label: 'Resources',
-    slug: '',
+    title: 'Demo',
+    slug: '/demo',
+  },
+];
+
+export const menuList = [
+  {
+    product: [
+      {
+        title: 'Features',
+        slug: '/features',
+      },
+      {
+        title: 'Prices',
+        slug: '/prices',
+      },
+      {
+        title: 'Demo',
+        slug: '/demo',
+      },
+    ],
   },
   {
-    label: 'Community',
-    slug: '',
+    company: [
+      {
+        title: 'About Us',
+        slug: '/about-us',
+      },
+      {
+        title: 'Privacy Policy',
+        slug: '/privacy-policy',
+      },
+      {
+        title: 'Careers',
+        slug: '/careers',
+      },
+      {
+        title: 'Terms of Service',
+        slug: '/terms-of-service',
+      },
+      {
+        title: 'FAQs',
+        slug: '/FAQs',
+      },
+    ],
+  },
+  {
+    resources: [
+      {
+        title: 'Coming Soon',
+        slug: '/coming-soon',
+      },
+      {
+        title: 'FAQs',
+        slug: '/FAQs',
+      },
+      {
+        title: 'Blog',
+        slug: '/blog',
+      },
+      {
+        title: 'Best Practices',
+        slug: '/best-practices',
+      },
+    ],
+  },
+  {
+    community: [
+      {
+        title: 'DevOps Community',
+        slug: '/devops-community',
+      },
+      {
+        title: 'News Room',
+        slug: '/news-room',
+      },
+      {
+        title: 'Podcasts',
+        slug: '/podcasts',
+      },
+    ],
   },
 ];
