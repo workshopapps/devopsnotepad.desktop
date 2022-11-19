30731d0412bb456b416c617f580fc758781a5726
osuji chiaka <chiakajunior@yahoo.com>
2022-11-19 09:19:33 +0300

    updated and completed the FAQ page

---
+++ frontend_website/src/Pages/Faq/Component/Ans.js
@@ -0,0 +1,16 @@
+const Ans = ({answer}) => {
+    return(
+        <div>
+            {answer}
+            {/* {btnName === 'btn1'? 'OpsPad is a note-taking and documentation application that allows management and server tracking features.': ''}
+            {btnName === 'btn2'? 'OpsPad is designed exclusively for DevOps engineers to use as they manage and keep track of servers.': ''}
+            {btnName === 'btn3'? 'No, there is no need to pre-install any software.': ''}
+            {btnName === 'btn4'? 'Yes, it is absolutely secure. All transmissions take place through Industry Standard Security using SSL/256-bit AES encryption protocols.': ''}
+            {btnName === 'btn5'? 'There are no specific restrictions on the number of notes you can create, access or edit. However, you may be limited by the hardware restrictions of your device. ': ''}
+            {/* {btnName === 'btn3'? 'No, there is no need to pre-install any software.': ''} */}
+             
+        </div>
+    )
+}
+
+export default Ans;
