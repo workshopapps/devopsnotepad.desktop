56a1a39e0bad06d304ed7a3c5f8b1ef98c8bde53
osuji chiaka <chiakajunior@yahoo.com>
2022-11-19 20:17:05 +0300

    solved the conflict errors

--- frontend_website/src/Pages/About/About.js
+++ frontend_website/src/Pages/About/About.js
@@ -5,7 +5,6 @@ import { BsArrowRightShort } from 'react-icons/bs';
 import womanwhite from './images/womanwhite.png';
 import womanblack from './images/womanblack.png';
 import classes from './About.module.css';
-
 export const About = () => {
   return (
     <div className={classes.container}>
