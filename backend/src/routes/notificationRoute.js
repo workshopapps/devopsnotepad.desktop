9735e05c54a2bfc97a8f6a8a62090e001d3b6413
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-20 16:52:40 +0100

    feat(BE-5): resolved merged conflicts

--- backend/src/routes/notificationRoute.js
+++ backend/src/routes/notificationRoute.js
@@ -4,5 +4,6 @@ import NotificationController from "../controllers/NotificationController.js";
 const router = express.Router();
 
 router.post("/:serverId/notifications", NotificationController.create);
+router.get("/:serverId/notifications", NotificationController.get);
 
 export default router;
