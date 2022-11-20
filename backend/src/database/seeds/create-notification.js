b0088d03f68f25af17b315c23b2bf05a6a00adf2
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-20 18:01:55 +0100

    feat(BE-5): added seed files

--- backend/src/database/seeds/create-notification.js
+++ backend/src/database/seeds/create-notification.js
@@ -6,12 +6,12 @@
   // Deletes ALL existing entries
   // await knex("notifications").del();
   await knex("notifications").insert([
-    {id: 1, notificationLog: "easfhgrehegj//get/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2},
-    {id: 2,  notificationLog: "ekfsdfwoeu8428sf ://hgrehegj//post/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
-    {id: 3,  notificationLog: "hds382942r-32724JRWPIFW4RW ://egwf839//put/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
-    {id: 4,  notificationLog: "ekfsdfwoeu8428sf ://lorem upsuarhgr//patch/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2, createdAt: knex.raw(`NOW() - INTERVAL 1 WEEK`)},
-    {id: 5,  notificationLog: "ekfsdfwoeu8428sf :lfvbag483ry43o//delete/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
-    {id: 6,  notificationLog: "ekfsdfwoeu8428sf fgwer329t/post/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2, createdAt: knex.raw(`NOW() - INTERVAL 1 MONTH`)},
-    {id: 7,  notificationLog : "ekfsdfwoeu842dnveihfiewfrqknvsV RWG3  T59UYPRS//get/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2},
+    {notificationId: 1, log: "easfhgrehegj//get/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2},
+    {notificationId: 2,  log: "ekfsdfwoeu8428sf ://hgrehegj//post/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
+    {notificationId: 3,  log: "hds382942r-32724JRWPIFW4RW ://egwf839//put/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
+    {notificationId: 4,  log: "ekfsdfwoeu8428sf ://lorem upsuarhgr//patch/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2, created_at: knex.raw(`NOW() - INTERVAL 1 WEEK`)},
+    {notificationId: 5,  log: "ekfsdfwoeu8428sf :lfvbag483ry43o//delete/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 1},
+    {notificationId: 6,  log: "ekfsdfwoeu8428sf fgwer329t/post/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2, created_at: knex.raw(`NOW() - INTERVAL 1 MONTH`)},
+    {notificationId: 7,  log : "ekfsdfwoeu842dnveihfiewfrqknvsV RWG3  T59UYPRS//get/----ngkfgrfjvrwr/lrojegje/getnkidnfigwhuwe", serverId: 2},
   ]);
 };
--- backend/src/database/seeds/create-server.js
+++ backend/src/database/seeds/create-server.js
@@ -4,7 +4,7 @@
  */
 export async function seed(knex) {
     await knex("servers").insert([
-        {id: "1", name: "server1", ipAddress: "localhost"},
-        {id: "2", name: "server2", ipAddress: "127:0:0:1"},
+        {id: 1, name: "server1", ipAddress: "localhost", deviceId: "eugr238yehfwq"},
+        {id: 2, name: "server2", ipAddress: "127:0:0:1", deviceId: "127gudbweif29"},
     ]);
 }
