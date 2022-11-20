fb0503d8458537aad2ac3bea354afb79d47b8965
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-20 15:37:41 +0100

    fix(BE-5): Fixed Notification issue

---
+++ backend/src/database/repositories/NotificationRepo.js
@@ -0,0 +1,33 @@
+import connection from "../setup";
+import APIFeatures from "../../utils/apiFeatures.js";
+
+export default class NotificationRepo {
+    static getNotifications = async (id, query) => {
+        const dbQuery = connection("notifications")
+            .where("serverId", id);
+        return new APIFeatures(dbQuery, query)
+            .paginate()
+            .sort()
+            .query; 
+    };
+
+    static getWeeklyNotifications = async (id, query) => {
+        const dbQuery = connection("notifications")
+            .where("serverId", id)
+            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 WEEK AND NOW()`));
+        return new APIFeatures(dbQuery, query)
+            .paginate()
+            .sort()
+            .query;
+    };
+
+    static getMonthlyNotifications = async (id, query) => {
+        const dbQuery = connection("notifications")
+            .where("serverId", id)
+            .andWhere(connection.raw(`createdAt BETWEEN NOW() - INTERVAL 1 MONTH AND NOW()`));
+        return new APIFeatures(dbQuery, query)
+            .paginate()
+            .sort()
+            .query;
+    };
+}
