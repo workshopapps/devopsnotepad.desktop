c16dcfb5e1a413a5b0d1305079046273fab18c63
Joel Ojerinde <joelojerinde@gmail.com>
2022-11-21 00:47:29 +0100

    cleanup: clean unsued codes and clear duplicates imports

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import LandingPage from './Pages/LandingPage/LandingPage';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import LandingPage from './Pages/Landing page/LandingPage';
 import { ErrorBoundary } from 'react-error-boundary';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import { About } from './Pages/About/About';
@@ -13,16 +13,15 @@ import OurTeam from './Pages/TeamPage/OurTeam';
 import AllTeam from './Pages/TeamPage/AllTeam';
 import ComingSoon from './Pages/ComingSoon/ComingSoon';
 import CareerPage from './Pages/CareerPage/CareerPage';
+import classes from './App.module.css';
 import Features from './Pages/Features/Features';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/SignUp/SignUp';
 import NewsRoom from './Pages/NewsRoom/NewsRoom';
 import DemoPage from './Pages/DemoPage/DemoPage';
 import ErrorPage from './Pages/404Page/ErrorPage';
-import Blog from './Pages/Blog/Blog';
-
-import classes from './App.module.css';
 import DevopsCommunity from './Pages/DevopsCompunity/DevopsCommunity';
+import Blog from './Pages/Blog/Blog';
 import Csr from './Pages/Csr/Csr';
 
 // Error Boundary FallbackComponent: This is the function that will be called whenever the errorboundary component caught an error
@@ -52,12 +51,9 @@ function App() {
       >
         <Routes>
           <Route path='/' exact element={<LandingPage />} />
-          <Route path='/faq' exact element={<Faq />} />
-          <Route path='/coming-soon' exact element={<ComingSoon />} />
           <Route path='/integrations' element={<Integration />} />
-          <Route path='/devops-community' exact element={<DevopsCommunity />} />
-          <Route path='/csr' exact element={<Csr />} />
-          <Route path='/blog' exact element={<Blog />} />
+          <Route path='/career' exact element={<CareerPage />} />
+          <Route path='/faq' exact element={<Faq />} />
           <Route path='/signup' exact element={<SignUp />} />
           <Route path='/login' exact element={<Login />} />
           <Route path='/careers' exact element={<CareerPage />} />
@@ -67,7 +63,11 @@ function App() {
           <Route path='/news-room' exact element={<NewsRoom />} />
           <Route path='/demo' exact element={<DemoPage />} />
           <Route path='/our-team' exact element={<OurTeam />} />
+          <Route path='/devops-community' exact element={<DevopsCommunity />} />
+          <Route path='/blog' exact element={<Blog />} />
           <Route path='/our-team/all-teams' exact element={<AllTeam />} />
+          <Route path='/csr' exact element={<Csr />} />
+          <Route path='/coming-soon' exact element={<ComingSoon />} />
 
           {/* This will be rendered on going to a path that does not exist in any of the paths above */}
           <Route path='*' exact element={<ErrorPage />} />
