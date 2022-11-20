322fedc14ead84f1917ece213403a4ee8fdcc2e2
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-20 16:52:40 +0100

    feat(BE-5): resolved merged conflicts

--- backend/src/routes/serverRoute.js
+++ backend/src/routes/serverRoute.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import ServerController from '../controllers/ServerController.js';
-import NotificationController from "../controllers/notificationController.js";
 
 const router = express.Router();
-
-router.get("/:serverId/notifications", NotificationController.get);
 
 router.post('/', ServerController.create);
 router.patch('/', ServerController.update);
