95c1bebbbf358b28db4303fa27015f5c167e595a
Anthony <olympnetwork@gmail.com>
2022-11-20 07:02:43 +0100

    feat(BE-7): validate data on endpoints

--- backend/src/controllers/ServerController.js
+++ backend/src/controllers/ServerController.js
@@ -3,10 +3,19 @@ import deleteSeversById from '../services/server/delete.js';
 import update from '../services/server/update.js';
 import getAllServers from '../services/server/getAll.js';
 import readOne from '../services/server/readOne.js';
+import { validatePayload } from '../utils/index.js';
 
 export default class ServerController {
   static create = async (req, res, next) => {
     try {
+      /**
+       * Validate Request
+       */
+      const errors = validatePayload(req);
+
+      // Update this latter
+      if (errors && Object.keys(errors).length > 0) throw errors;
+
       const result = await create(req.body);
 
       res.send({
@@ -44,6 +53,14 @@ export default class ServerController {
 
   static update = async (req, res, next) => {
     try {
+      /**
+       * Validate Request
+       */
+      const errors = validatePayload(req);
+
+      // Update this latter
+      if (errors && Object.keys(errors).length > 0) throw errors;
+
       const result = await update(req.body);
       res.send({
         success: true,
--- backend/src/routes/serverRoute.js
+++ backend/src/routes/serverRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import ServerController from '../controllers/ServerController.js';
+import { createServerValidator, updateServerValidator } from '../validators/serverValidators.js';
 
 const router = express.Router();
 
-router.post('/', ServerController.create);
-router.patch('/', ServerController.update);
+router.post('/', createServerValidator, ServerController.create);
+router.patch('/', updateServerValidator, ServerController.update);
 router.get('/', ServerController.getAllServers);
 router.get('/:serverId', ServerController.readOne);
 router.delete('/', ServerController.deleteServersById);
