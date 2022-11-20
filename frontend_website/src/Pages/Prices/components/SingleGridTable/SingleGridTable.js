8f835710b55d6974f49b849dbe9d187137dbf5d3
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-20 16:47:37 +0100

    responsive: made the 'compare prices' table fully responsive

---
+++ frontend_website/src/Pages/Prices/components/SingleGridTable/SingleGridTable.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import GridData from '../../GridData';
+import './SingleGridTable.css';
+import bad from '../../x.svg';
+import good from '../../good.svg';
+
+const SingleGridTable = () => {
+  const [showBundle, setShowBundle] = useState({
+    starter: true,
+    professional: false,
+    organization: false,
+  });
+
+  return (
+    <div className="single-table-container">
+      <ul>
+        <li
+          onClick={() =>
+            setShowBundle({
+              starter: true,
+              professional: false,
+              organization: false,
+            })
+          }
+        >
+          Starter
+        </li>
+        <li
+          onClick={() =>
+            setShowBundle({
+              starter: false,
+              professional: true,
+              organization: false,
+            })
+          }
+        >
+          Professional
+        </li>
+        <li
+          onClick={() =>
+            setShowBundle({
+              starter: false,
+              professional: false,
+              organization: true,
+            })
+          }
+        >
+          Organization
+        </li>
+      </ul>
+      <div className="single-grid-table">
+        <h9 className="grid-header">Unlimited Number of servers</h9>
+        <img className="grid-item bad" src={good} alt="" />
+        <h9 className="grid-header">Note</h9>
+        <img className="grid-item bad" src={bad} alt="" />
+        <h9 className="grid-header">Unlimited Number of servers</h9>
+        <img className="grid-item bad" src={bad} alt="" />
+        <h9 className="grid-header">Unlimited Number of servers</h9>
+        <img className="grid-item bad" src={bad} alt="" />
+        <h9 className="grid-header">Unlimited Number of servers</h9>
+        <img className="grid-item bad" src={bad} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default SingleGridTable;
