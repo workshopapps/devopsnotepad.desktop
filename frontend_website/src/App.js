657b7a925dad0bee85df748e17126cc4e8a80418
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-21 20:48:08 +0100

    app updated

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './Component/Footer/Footer';
 import Navigation from './Component/Navigation/Navigation';
 import Faq from './Pages/Faq/Faq';
+import Csr from './Pages/Csr/Csr';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         {/* <Route path="/" exact element={<Integration />} /> */}
         <Route path="/faq" exact element={<Faq />} />
+        <Route path="/csr" exact element={<Csr />} />
       </Routes>
       <Footer />
     </React.Fragment>
