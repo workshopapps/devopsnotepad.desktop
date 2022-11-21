28c813491b2ed813f26f9dc1b7fd927b4649173f
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-20 20:49:37 -0800

    Add data

--- frontend_website/src/Pages/Partner/data/PartnerCardData.js
+++ frontend_website/src/Pages/Partner/data/PartnerCardData.js
@@ -4,21 +4,24 @@ import Zoey from '../assets/zoey.png';
 
 
 export const PartnerCardData = [
-    {
+    {   
+        id: "1",
         img: Jane,
         name: "Jane Doe",
         position: "Lead developer, Zuri inc.",
         characteristics: "They make working fun",
         text: "I have had the opportunity of working with several companies before now, but Opspad works differently."
     },
-    {
+    {   
+        id: "2",
         img: Micheal,
         name: "Mathew Lane",
         position: "Personnel Manager,Data point",
         characteristics: "Great Teamwork",
         text: "Working inteams is not always easy to cope with, but the Opspad team is one i am willing to owork with over and over again."
     },
-    {
+    {   
+        id: "3",
         img: Zoey,
         name: "Zoey Cruz",
         position: "Marketing Lead, Soreline ltd",
