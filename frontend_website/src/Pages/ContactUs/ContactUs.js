45f56635c67260defe36fd827660760e9ad85114
Joel Ojerinde <joelojerinde@gmail.com>
2022-11-23 00:34:00 +0100

    cleanup: cleanup unsed variable, duplicate imports

--- frontend_website/src/Pages/ContactUs/ContactUs.js
+++ frontend_website/src/Pages/ContactUs/ContactUs.js
@@ -1,267 +1,264 @@
 import React from 'react';
 import styles from './ContactUs.module.css';
-// import email from './images/email.png';
-// import phone from './images/phone.png';
 import support from './images/support.png';
 import sales from './images/sales.png';
 import press from './images/press.png';
 import developer from './images/developer.png';
 import { useFormik } from 'formik';
 import { contactUsSchema } from './Schema';
-// import { Link } from "react-router-dom";
-import { RiCloseCircleFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { RiCloseCircleFill } from 'react-icons/ri';
 
 const onSubmit = async (values, actions) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  alert('Form Submitted');
-  actions.resetForm();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    alert('Form Submitted');
+    actions.resetForm();
 };
 
 const ContactUs = () => {
-  const {
-    values,
-    errors,
-    touched,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    isSubmitting,
-  } = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      phone: '',
-      subject: '',
-      message: '',
-    },
+    const {
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+    } = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            phone: '',
+            subject: '',
+            message: '',
+        },
 
-    validationSchema: contactUsSchema,
-    onSubmit,
-  });
+        validationSchema: contactUsSchema,
+        onSubmit,
+    });
 
-  return (
-    <div>
-      <section className={styles.container}>
-        <section className={styles.contact}>
-          <p className={styles.contactUs}>CONTACT US</p>
+    return (
+        <div>
+            <section className={styles.container}>
+                <section className={styles.contact}>
+                    <p className={styles.contactUs}>CONTACT US</p>
 
-          <p className={styles.inTouch}>Get in Touch Today!</p>
+                    <p className={styles.inTouch}>Get in Touch Today!</p>
 
-          <p className={styles.help}>
-            We want to hear from you. Let us know <br /> how we can help.
-          </p>
-        </section>
+                    <p className={styles.help}>
+                        We want to hear from you. Let us know <br/> how we can help.
+                    </p>
+                </section>
 
-        <section className={styles.support}>
-          <div className={styles.col1}>
-            <div className={styles.card}>
-              <img src={support} alt='' />
+                <section className={styles.support}>
+                    <div className={styles.col1}>
+                        <div className={styles.card}>
+                            <img src={support} alt='' />
 
-              <p className={styles.supportP}>Support</p>
+                            <p className={styles.supportP}>Support</p>
 
-              <p className={styles.assistance}>
-                If you’re having trouble with OpsPad, check out the FAQs section
-                or get assistance from other OpsPad users in our discussion
-                forums.
-              </p>
+                            <p className={styles.assistance}>
+                                If you’re having trouble with OpsPad, check out the FAQs section
+                                or get assistance from other OpsPad users in our discussion
+                                forums.
+                            </p>
 
-              <button className={styles.btns}>
-                <Link to='/faq'>GET SUPPORT</Link>
-              </button>
-            </div>
+                            <button className={styles.btns}>
+                                <Link to='/faq' style={{ color: 'white', textDecoration: 'none' }}>GET SUPPORT</Link>
+                            </button>
+                        </div>
 
-            <div className={styles.card}>
-              <img src={sales} alt='' />
+                        <div className={styles.card}>
+                            <img src={sales} alt='' />
 
-              <p className={styles.supportP}>
-                Sales <br />{' '}
-              </p>
+                            <p className={styles.supportP}>
+                                Sales <br />{' '}
+                            </p>
 
-              <p className={styles.assistance}>
-                Are you interested in the OpsPad? Get in touch with an OpsPad
-                sales representative. <br /> <br />{' '}
-              </p>
+                            <p className={styles.assistance}>
+                                Are you interested in the OpsPad? Get in touch with an OpsPad
+                                sales representative. <br /> <br />{' '}
+                            </p>
 
-              <button
-                className={styles.btns}
-                onClick={() =>
-                  (window.location = 'mailto:sales@teamsandpaper.com')
-                }
-              >
-                CONTACT US
-              </button>
-            </div>
-          </div>
+                            <button
+                                className={styles.btns}
+                                onClick={() =>
+                                    (window.location = 'mailto:sales@teamsandpaper.com')
+                                }
+                            >
+                                CONTACT US
+                            </button>
+                        </div>
+                    </div>
 
-          <div className={styles.col1}>
-            <div className={styles.card}>
-              <img src={press} alt='' />
+                    <div className={styles.col1}>
+                        <div className={styles.card}>
+                            <img src={press} alt='' />
 
-              <p className={styles.supportP}>Press & Partnership Enquiries</p>
+                            <p className={styles.supportP}>Press & Partnership Enquiries</p>
 
-              <p className={styles.assistance}>
-                For press and partnerships enquiries, Click on the button below.
-              </p>
+                            <p className={styles.assistance}>
+                                For press and partnerships enquiries, Click on the button below.
+                            </p>
 
-              <button
-                className={styles.btns}
-                onClick={() =>
-                  (window.location = 'mailto:partnerships@teamsandpaper.com')
-                }
-              >
-                CONTACT US
-              </button>
-            </div>
+                            <button
+                                className={styles.btns}
+                                onClick={() =>
+                                    (window.location = 'mailto:partnerships@teamsandpaper.com')
+                                }
+                            >
+                                CONTACT US
+                            </button>
+                        </div>
 
-            <div className={styles.card}>
-              <img src={developer} alt='' />
+                        <div className={styles.card}>
+                            <img src={developer} alt='' />
 
-              <p className={styles.supportP}>
-                Developer <br /> <br />{' '}
-              </p>
+                            <p className={styles.supportP}>
+                                Developer <br /> <br />{' '}
+                            </p>
 
-              <p className={styles.assistance}>
-                If you are interested in an integration, please refer to our
-                Developer / API Support. <br />{' '}
-              </p>
+                            <p className={styles.assistance}>
+                                If you are interested in an integration, please refer to our
+                                Developer / API Support. <br />{' '}
+                            </p>
 
-              <button className={styles.btns}>
-                <Link to='/integrations'>INTEGRATIONS</Link>
-              </button>
-            </div>
-          </div>
-        </section>
+                            <button className={styles.btns}>
+                                <Link to='/integrations' style={{ color: 'white', textDecoration: 'none' }}>INTEGRATIONS</Link>
+                            </button>
+                        </div>
+                    </div>
+                </section>
 
-        <section className={styles.wrap}>
-          <p className={styles.reachOut}> Need More Help?</p>
+                <section className={styles.wrap}>
+                    <p className={styles.reachOut}> Need More Help?</p>
 
-          <p className={styles.canHelp}>
-            We want to hear from you. Let us know how <br /> we can help.
-          </p>
-        </section>
+                    <p className={styles.canHelp}>
+                        We want to hear from you. Let us know how <br /> we can help.
+                    </p>
+                </section>
 
-        <section className={styles.wrapper}>
-          <form action='#' autoComplete='off' onSubmit={handleSubmit}>
-            <div className={styles.row}>
-              <div className={styles.col}>
-                <label for='firstName'>First Name</label>
-                <input
-                  type='text'
-                  id='firstName'
-                  placeholder='What’s your first name?'
-                  className={styles.end}
-                  required
-                  onChange={handleChange}
-                  value={values.firstName}
-                  onBlur={handleBlur}
-                />
+                <section className={styles.wrapper}>
+                    <form action='#' autoComplete='off' onSubmit={handleSubmit}>
+                        <div className={styles.row}>
+                            <div className={styles.col}>
+                                <label for='firstName'>First Name</label>
+                                <input
+                                    type='text'
+                                    id='firstName'
+                                    placeholder='What’s your first name?'
+                                    className={styles.end}
+                                    required
+                                    onChange={handleChange}
+                                    value={values.firstName}
+                                    onBlur={handleBlur}
+                                />
 
-                {errors.firstName && touched.firstName && (
-                  <p className={styles.errorMsg}>
-                    {' '}
-                    <RiCloseCircleFill /> {errors.firstName}{' '}
-                  </p>
-                )}
-              </div>
+                                {errors.firstName && touched.firstName && (
+                                    <p className={styles.errorMsg}>
+                                        {' '}
+                                        <RiCloseCircleFill /> {errors.firstName}{' '}
+                                    </p>
+                                )}
+                            </div>
 
-              <div className={styles.col}>
-                <label for='lastName'>Last Name</label>
-                <input
-                  type='text'
-                  id='lastName'
-                  placeholder='What’s your last name?'
-                  className={styles.start}
-                  required
-                  onChange={handleChange}
-                  value={values.lastName}
-                  onBlur={handleBlur}
-                />
+                            <div className={styles.col}>
+                                <label for='lastName'>Last Name</label>
+                                <input
+                                    type='text'
+                                    id='lastName'
+                                    placeholder='What’s your last name?'
+                                    className={styles.start}
+                                    required
+                                    onChange={handleChange}
+                                    value={values.lastName}
+                                    onBlur={handleBlur}
+                                />
 
-                {errors.lastName && touched.lastName && (
-                  <p className={styles.errorMsg}>
-                    {' '}
-                    <RiCloseCircleFill /> {errors.lastName}{' '}
-                  </p>
-                )}
-              </div>
-            </div>
+                                {errors.lastName && touched.lastName && (
+                                    <p className={styles.errorMsg}>
+                                        {' '}
+                                        <RiCloseCircleFill /> {errors.lastName}{' '}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
 
-            <div className={styles.row}>
-              <div className={styles.col}>
-                <label for='phone'>Phone Number</label>
-                <input
-                  type='number'
-                  id='phone'
-                  placeholder='What’s your phone number?'
-                  className={styles.end}
-                  required
-                  onChange={handleChange}
-                  value={values.phone}
-                  onBlur={handleBlur}
-                />
+                        <div className={styles.row}>
+                            <div className={styles.col}>
+                                <label for='phone'>Phone Number</label>
+                                <input
+                                    type='number'
+                                    id='phone'
+                                    placeholder='What’s your phone number?'
+                                    className={styles.end}
+                                    required
+                                    onChange={handleChange}
+                                    value={values.phone}
+                                    onBlur={handleBlur}
+                                />
 
-                {errors.phone && touched.phone && (
-                  <p className={styles.errorMsg}>
-                    {' '}
-                    <RiCloseCircleFill /> {errors.phone}{' '}
-                  </p>
-                )}
-              </div>
+                                {errors.phone && touched.phone && (
+                                    <p className={styles.errorMsg}>
+                                        {' '}
+                                        <RiCloseCircleFill /> {errors.phone}{' '}
+                                    </p>
+                                )}
+                            </div>
 
-              <div className={styles.col}>
-                <label for='subject'>Subject</label>
-                <input
-                  type='text'
-                  placeholder='How can we help?'
-                  id='subject'
-                  className={styles.start}
-                  required
-                  onChange={handleChange}
-                  value={values.subject}
-                  onBlur={handleBlur}
-                />
+                            <div className={styles.col}>
+                                <label for='subject'>Subject</label>
+                                <input
+                                    type='text'
+                                    placeholder='How can we help?'
+                                    id='subject'
+                                    className={styles.start}
+                                    required
+                                    onChange={handleChange}
+                                    value={values.subject}
+                                    onBlur={handleBlur}
+                                />
 
-                {errors.subject && touched.subject && (
-                  <p className={styles.errorMsg}>
-                    {' '}
-                    <RiCloseCircleFill /> {errors.subject}{' '}
-                  </p>
-                )}
-              </div>
-            </div>
+                                {errors.subject && touched.subject && (
+                                    <p className={styles.errorMsg}>
+                                        {' '}
+                                        <RiCloseCircleFill /> {errors.subject}{' '}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
 
-            <div className={styles.col}>
-              <label for='message'>Your Message</label>
-              <textarea
-                id='message'
-                rows='4'
-                className={styles.message}
-                placeholder='Hello there, I would like to talk about how to...'
-                required
-                onChange={handleChange}
-                value={values.message}
-                onBlur={handleBlur}
-              ></textarea>
+                        <div className={styles.col}>
+                            <label for='message'>Your Message</label>
+                            <textarea
+                                id='message'
+                                rows='4'
+                                className={styles.message}
+                                placeholder='Hello there, I would like to talk about how to...'
+                                required
+                                onChange={handleChange}
+                                value={values.message}
+                                onBlur={handleBlur}
+                            ></textarea>
 
-              {errors.message && touched.message && (
-                <p className={styles.errorMsg}>
-                  {' '}
-                  <RiCloseCircleFill /> {errors.message}{' '}
-                </p>
-              )}
-            </div>
+                            {errors.message && touched.message && (
+                                <p className={styles.errorMsg}>
+                                    {' '}
+                                    <RiCloseCircleFill /> {errors.message}{' '}
+                                </p>
+                            )}
+                        </div>
 
-            <div>
-              <button className={styles.btn} disabled={isSubmitting}>
-                Send Message
-              </button>
-            </div>
-          </form>
-        </section>
-      </section>
-    </div>
-  );
+                        <div>
+                            <button className={styles.btn} disabled={isSubmitting}>
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </section>
+        </div>
+    );
 };
 
 export default ContactUs;
