d46becc5e2dd2ccda56c3fa5769876cb49c44585
Chima <orlandofays3@gmail.com>
2022-11-23 19:06:38 +0100

    resloved merge conflict on notification tests

--- backend/test/integration/Notification.test.js
+++ backend/test/integration/Notification.test.js
@@ -3,16 +3,12 @@ import supertest from "supertest";
 import app from "../../src/initialize.js";
 import Documentator from "../../src/utils/documentator/index.js";
 import connection from "../../src/database/setup.js";
-<<<<<<< HEAD
 import ServerRepo from "../../src/database/repositories/ServerRepo.js";
-=======
->>>>>>> 0840880f6416fea392b33501883bb1def77c9b78
 
 export const request = supertest.agent(app);
 
 export const docmaker = Documentator.getInstance();
 
-<<<<<<< HEAD
 const getNotificationByLog = async (log) => {
     return connection("notifications").where("log", log).first();
 };
@@ -73,41 +69,10 @@ describe("Notification", () => {
         assert.equal(res.status, 200);
         assert.equal(notifications.length, 1);
         assert.equal(Object.keys(notifications[0]).length, 5);
-=======
-
-// Deletes every record from servers table before any test is run to avoid collisions.
-before(async () => {
-    await connection.raw("delete from notifications");
-});
-
-before(async () => {
-    await connection.raw("delete from servers");
-});
-
-before(async () => {
-    await connection.seed.run({ specific: "create-server.js" });
-    await connection.seed.run({ specific: "create-notification.js" });
-    console.log(`database seeded`);
-});
-
-
-
-describe("Notification", () => {
-    
-    it("should get new notifications and check default ordering", async () => {
-        const res = await request.get("/server/2/notifications");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);
-        assert.equal(Object.keys(notifications[0]).length, 5);
-        assert.include(notifications[0].notificationId, 1);
->>>>>>> 0840880f6416fea392b33501883bb1def77c9b78
         docmaker.addEndpoint(res);
     });
 
     it("should check pagination when getting notification", async () => {
-<<<<<<< HEAD
         const res = await request.get(`/server/${serverId}/notifications?page=1&limit=2`);
 
         let notifications = res.body.notifications;
@@ -140,52 +105,6 @@ describe("Notification", () => {
         let notifications = res.body.notifications;
         assert.equal(res.status, 200);
         assert.equal(notifications.length, 1);
-=======
-        const res = await request.get("/server/2/notifications?page=2&limit=2");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 2);
-        assert.include(notifications[0].notificationId, 4); 
-        docmaker.addEndpoint(res);   
-    });
-
-    it("should return notifications for the last 1 week and check ordering in asc order", async () => {
-        const res = await request.get("/server/2/notifications?range=weekly&orderBy=created_at+asc");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 3);
-        assert.include(notifications[0].notificationId, 4);  
-        docmaker.addEndpoint(res);  
-    });
-
-    it("should return notifications for the last 1 month", async () => {
-        const res = await request.get("/server/2/notifications?range=monthly");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);
-        assert.include(notifications[3].notificationId, 6);    
-        docmaker.addEndpoint(res);
-    });
-
-    it("should return empty notification", async () => {
-        const res = await request.get("/server/3/notifications");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 0);
-        docmaker.addEndpoint(res);  
-    });
-
-    it("should return default notifications if query parameter is invalid", async () => {
-        const res = await request.get("/server/2/notifications?pg=2&ordevfye=5");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);  
->>>>>>> 0840880f6416fea392b33501883bb1def77c9b78
         docmaker.addEndpoint(res);
     });
 });
