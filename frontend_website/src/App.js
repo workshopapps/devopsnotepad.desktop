611ed6cb83281f8d37ff273afc8d285cef27145c
drizzy009 <yusid03@gmail.com>
2022-11-23 09:25:37 +0100

    fix; conflicts

--- frontend_website/src/App.js
+++ frontend_website/src/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import "./App.css";
-import React from "react";
-import Footer from "./Component/Footer/Footer";
-import Header from "./Component/Header/Header";
-=======
 import React from 'react';
-import LandingPage from './Pages/LandingPage/LandingPage';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import { ErrorBoundary } from 'react-error-boundary';
+
+import LandingPage from './Pages/LandingPage/LandingPage';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import { About } from './Pages/About/About';
 import Footer from './Component/Footer/Footer';
@@ -20,7 +15,6 @@ import AllTeam from './Pages/TeamPage/AllTeam';
 import ComingSoon from './Pages/ComingSoon/ComingSoon';
 import CareerPage from './Pages/CareerPage/CareerPage';
 import Partner from './Pages/Partner/Partner'
-import classes from './App.module.css'
 import Features from './Pages/Features/Features';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/SignUp/SignUp';
@@ -30,8 +24,10 @@ import Csr from './Pages/Csr/Csr';
 import ErrorPage from './Pages/404Page/ErrorPage';
 import DevopsCommunity from './Pages/DevopsCompunity/DevopsCommunity';
 import Blog from './Pages/Blog/Blog';
-import Csr from './Pages/Csr/Csr';
+import ForgetPassword from './Pages/ForgetPassword/ForgotPassword';
+import Podcast from './Pages/Podcast/Podcasts';
 
+import classes from './App.module.css'
 // Error Boundary FallbackComponent: This is the function that will be called whenever the errorboundary component caught an error
 const ErrorFallback = (props) => {
   return (
@@ -44,19 +40,13 @@ const ErrorFallback = (props) => {
     </div>
   );
 };
->>>>>>> 598017e8d7b926270ead112ef59ff3b6f1bae0e5
 
 function App() {
   const navigate = useNavigate();
 
   return (
     <React.Fragment>
-<<<<<<< HEAD
-      <Header />
-      <Footer />
-      {/* <LandingPage /> */}
-=======
-       <Navigation />
+      <Navigation />
 
       <ErrorBoundary
         FallbackComponent={ErrorFallback}
@@ -67,7 +57,6 @@ function App() {
         <Routes>
           <Route path='/' exact element={<LandingPage />} />
           <Route path='/integrations' element={<Integration />} />
-          <Route path='/career' exact element={<CareerPage />} />
           <Route path='/faq' exact element={<Faq />} />
           <Route path='/signup' exact element={<SignUp />} />
           <Route path='/login' exact element={<Login />} />
@@ -82,15 +71,16 @@ function App() {
           <Route path='/devops-community' exact element={<DevopsCommunity />} />
           <Route path='/blog' exact element={<Blog />} />
           <Route path='/our-team/all-teams' exact element={<AllTeam />} />
-          <Route path='/partner' exact element={<Partner/>} />
-          <Route path='/csr' exact element={<Csr/>} />
+          <Route path='/partners' exact element={<Partner />} />
+          <Route path='/csr' exact element={<Csr />} />
+          <Route path='/forgot-password' exact element={<ForgetPassword />} />
+          <Route path='/podcasts' exact element={<Podcast />} />
 
           {/* This will be rendered on going to a path that does not exist in any of the paths above */}
           <Route path='*' exact element={<ErrorPage />} />
         </Routes>
       </ErrorBoundary>
       <Footer />
->>>>>>> 598017e8d7b926270ead112ef59ff3b6f1bae0e5
     </React.Fragment>
   );
 }
