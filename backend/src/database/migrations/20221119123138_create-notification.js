08b0e9b7cb605fef38b822679f27b723da84a9e2
Chima <orlandofays3@gmail.com>
2022-11-23 19:22:37 +0100

    resloved merge conflict on notification tests

--- backend/src/database/migrations/20221119123138_create-notification.js
+++ backend/src/database/migrations/20221119123138_create-notification.js
@@ -1,14 +1,8 @@
 export async function up(knex) {
     return knex.schema.createTable('notifications', table => {
-<<<<<<< HEAD
         table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
         table.uuid("serverId").notNullable();
         table.foreign("servers");
-=======
-        table.uuid("notificationId").primary().defaultTo(knex.schema.raw("(UUID())"));
-        table.uuid("serverId").defaultTo(knex.schema.raw("(UUID())"));
-        table.foreign("serverId");
->>>>>>> 0840880f6416fea392b33501883bb1def77c9b78
         table.string('log', 900).notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table
