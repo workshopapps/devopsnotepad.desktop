1b8f7a8b8b04e423e6cdc62cafdc8adbc8560a65
Chima <orlandofays3@gmail.com>
2022-11-23 18:53:59 +0100

    fix/BE-fix server id and notification id returning (UUID()) when creating new notifications and fix failing tests

--- backend/src/database/migrations/20221119123138_create-notification.js
+++ backend/src/database/migrations/20221119123138_create-notification.js
@@ -1,8 +1,8 @@
 export async function up(knex) {
     return knex.schema.createTable('notifications', table => {
-        table.uuid("notificationId").primary().defaultTo(knex.raw("(UUID())"));
-        table.uuid("serverId").defaultTo(knex.raw("(UUID())"));
-        table.foreign("serverId");
+        table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+        table.uuid("serverId").notNullable();
+        table.foreign("servers");
         table.string('log', 900).notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table
