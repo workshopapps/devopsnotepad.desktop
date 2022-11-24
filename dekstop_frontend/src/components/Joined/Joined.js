16327336c48de1086f851d5382f5d83f74746b89
Israel Omitiran <omitiranisrael@gmail.com>
2022-11-24 12:55:09 +0100

    add server ui to desktop

---
+++ dekstop_frontend/src/components/Joined/Joined.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Modal from '../Modal/Modal'
+import Server from '../Server/Server'
+import Sidebar from '../Sidebar/Sidebar'
+import "./Joined.css"
+
+const Joined = () => {
+    const [modal, setModal] = useState(false)
+
+    const open = () => {
+        setModal(true)
+      }
+      const close = () => {
+        setModal(false)
+      }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setModal(prev => !prev);
+        console.log(modal)
+    }
+
+  return (
+    <div className='join'>
+        <Sidebar/>
+        <Server handleSubmit={handleSubmit}/>
+        <Modal modal={modal} open={open} close={close}/>
+    </div>
+  )
+}
+
+export default Joined
