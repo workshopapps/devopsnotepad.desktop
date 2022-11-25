38a691ffb1bd49d5c6e410468186a3502f4fde93
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-25 12:36:21 +0100

    feat: delete modal added to the notes

--- dekstop_frontend/src/Pages/notes/Note.jsx
+++ dekstop_frontend/src/Pages/notes/Note.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import Sidenav from '../../Component/SideNav/SideNav';
 import notesStyle from './Note.module.css';
+import deleteImg from './assets/Vector (14).png';
+
+const style = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: '50%',
+	height: '50%',
+	borderRadius: '8px',
+	bgcolor: 'background.paper',
+	border: 0,
+	background: '#FFFFFF',
+	boxShadow: '0px 4px 29px rgba(0, 0, 0, 0.12)',
+	p: 4,
+};
 
 function Note() {
 	// State
+	const [open, setOpen] = React.useState(false);
+
 	const [formDisplay, setFormDisplay] = React.useState(false);
 
-	// Handle form display
+	// Handlers
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 	const handleFormShow = () => {
 		setFormDisplay((prev) => !prev);
 	};
@@ -67,8 +90,7 @@ function Note() {
 							>
 								Notes
 							</button>
-							<button type="button" className={notesStyle.notesContentwoBtn}
-							>
+							<button type="button" className={notesStyle.notesContentwoBtn}>
 								Passwords
 							</button>
 							<button type="button" className={notesStyle.notesContentwoBtn}>
@@ -78,11 +100,34 @@ function Note() {
 						{formDisplay ? (
 							<div className={notesStyle.notesFormDiv}>
 								<div className={notesStyle.notesFormIcons}>
-									<img src="Vectorc.png" alt='img' className={notesStyle.notesFormIcon}/>
-									<img src="Vectorr.png" alt='img' className={notesStyle.notesFormIcon}/>
-									<img src="Vectoor.png" alt='img' className={notesStyle.notesFormIcon}/>
-									<img src="Vectorrr.png" alt='img' className={notesStyle.notesFormIcon}/>
-									<img src="Vecctor.png" alt='img' id={notesStyle.deleteIcon} className={notesStyle.notesFormIcon}/>
+									<img
+										src="Vectorc.png"
+										alt="img"
+										className={notesStyle.notesFormIcon}
+									/>
+									<img
+										src="Vectorr.png"
+										alt="img"
+										className={notesStyle.notesFormIcon}
+									/>
+									<img
+										src="Vectoor.png"
+										alt="img"
+										className={notesStyle.notesFormIcon}
+									/>
+									<img
+										src="Vectorrr.png"
+										alt="img"
+										className={notesStyle.notesFormIcon}
+									/>
+									<img
+										src="Vecctor.png"
+										alt="img"
+										id={notesStyle.deleteIcon}
+										className={notesStyle.notesFormIcon}
+										onMouseDownCapture={handleOpen}
+										onFocus={handleOpen}
+									/>
 								</div>
 								<p className={notesStyle.notesLastEdit}>Last edit</p>
 								<form className={notesStyle.notesForm}>
@@ -92,12 +137,45 @@ function Note() {
 										className={notesStyle.notesFormInput}
 									/>
 								</form>
-								
 							</div>
 						) : null}
 					</div>
 				</div>
 			</div>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<Typography id="modal-modal-title" variant="h6" component="h2">
+						<div className={notesStyle.notesDeleteDiv}>
+							<img
+								src={deleteImg}
+								alt="img"
+								className={notesStyle.notesDeleteImg}
+							/>
+							<p className={notesStyle.notesDeleteNotification}>
+								Are you sure you want too delete notes? Notes deleted cannot be retrieved
+							</p>
+							<div className={notesStyle.notesDeleteBtnDiv}>
+								<button
+									type="button"
+									className={notesStyle.notesDeleteBtn}
+								>
+									Cancel
+								</button>
+								<button type="button" 
+									id={notesStyle.notesDeleteBtn}
+									className={notesStyle.notesDeleteBtn}>
+									Delete
+								</button>
+							</div>
+						</div>
+					</Typography>
+				</Box>
+			</Modal>
 		</div>
 	);
 }
