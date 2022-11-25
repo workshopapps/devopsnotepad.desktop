322beb144785a784910f190afe0d8326e9a16b93
drizzy009 <yusid03@gmail.com>
2022-11-25 08:59:21 +0100

    add: new update

--- dekstop_frontend/src/Pages/Settings/components/toggle.jsx
+++ dekstop_frontend/src/Pages/Settings/components/toggle.jsx
@@ -4,12 +4,12 @@ import styled from "styled-components";
 const ToggleContainer = styled.button`
   font-size: .3rem;
   width: 5.5em;
-  border: 0.125em solid ${({ theme: { text } }) => text};
+  border: 0.125em solid ${({ theme: { toggle } }) => toggle};
   border-radius: 1.5em;
   margin: 0;
   padding: 0.125em;
   overflow: hidden;
-  background: ${({ theme: { body } }) => body};
+  background: ${({ theme: { toggle } }) => toggle};
   cursor: pointer;
   transition: all 0.3s linear;
 `;
@@ -18,7 +18,7 @@ const Switch = styled.div`
   width: 2.5em;
   height: 2.5em;
   border-radius: 50%;
-  background-color: ${({ theme: { text } }) => text};
+  background-color: ${({ theme: { toggle2 } }) => toggle2};
   position: relative;
   transform: ${({ theme: { name } }) =>
     name === "light" ? "translateX(0)" : "translateX(2.5em)"};
