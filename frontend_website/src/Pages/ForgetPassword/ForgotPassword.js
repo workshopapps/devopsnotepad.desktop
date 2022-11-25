38c051f818b56a5a5298ce8690facbe1bded7a82
MITCHYUGAN <denmichigan123@gmail.com>
2022-11-25 12:27:53 +0100

    Fixed and Inplemented

--- frontend_website/src/Pages/ForgetPassword/ForgotPassword.js
+++ frontend_website/src/Pages/ForgetPassword/ForgotPassword.js
@@ -1,28 +1,24 @@
-import React from "react";
-import forgetStyles from "../ForgetPassword/ForgetPassword.module.css"
-import { Routes, Route } from "react-router-dom";
-import Reset from "./Reset";
-import CheckEmail from "./CheckEmail";
-import Verification from "./Verification";
-import NewPassword from "./NewPassword";
-import Success from "./Success";
-
-
+import React from 'react';
+import forgetStyles from '../ForgetPassword/ForgetPassword.module.css';
+import { Routes, Route } from 'react-router-dom';
+import Reset from './Reset';
+import CheckEmail from './CheckEmail';
+import Verification from './Verification';
+import NewPassword from './NewPassword';
+import Success from './Success';
 
 const ForgetPassword = () => {
-    return ( 
-        <div className={forgetStyles.ForgetPassword}>
-            <Routes>
-                <Route path="/" element={<Reset/>}/>
-                <Route path="/checkemail" element={<CheckEmail/>}/>
-                <Route path="/verfication" element={<Verification/>}/>
-                <Route path="/newpassword" element={<NewPassword/>}/>
-                <Route path="/success" element={<Success/>}/>
-            </Routes>
+  return (
+    <div className={forgetStyles.ForgetPassword}>
+      <Routes>
+        <Route path='/' element={<Reset />} />
+        <Route path='/checkemail' element={<CheckEmail />} />
+        <Route path='/verfication' element={<Verification />} />
+        <Route path='/newpassword' element={<NewPassword />} />
+        <Route path='/success' element={<Success />} />
+      </Routes>
+    </div>
+  );
+};
 
-
-        </div>
-     );
-}
- 
 export default ForgetPassword;
