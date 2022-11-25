a86a4b78891763672c25f9c3cac4d698cae1dfd0
Blessing U <blessing986@users.noreply.github.com>
2022-11-25 12:09:19 +0000

    fix: updated code

--- dekstop_frontend/src/App.jsx
+++ dekstop_frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Notes from './Pages/notes/Notes';
+import Note from './Pages/notes/Note';
 import Home from './Pages/home/Home';
+import Notification from './Pages/Notification/Notification'
 
 function App() {
 	return (
 		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route exact path="/notes" element={<Notes />} />
-			</Routes>
+		  <Routes>
+			<Route  path="/" element={<Home />} />
+			<Route exact path="/note" element={<Note/>}/>
+			<Route exact path="/notification" element={<Notification />}/>
+		  </Routes>
 		</div>
 	);
 }
