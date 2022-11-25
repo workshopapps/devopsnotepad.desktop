2bdf07af5b6a8b4e7e5b9ec1353f12136bdb3956
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-25 19:59:25 +0100

    feat: css converted to module.css

--- frontend_website/src/Pages/Csr/Csr.js
+++ frontend_website/src/Pages/Csr/Csr.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Csr.css';
+import s from './Csr.module.css';
 
 const Csr = () => {
   const [isShown, setIsShown] = useState(false);
@@ -26,13 +26,13 @@ const Csr = () => {
     paddingRight: '150px',
   };
   return (
-    <div className='csrWrapper'>
-      <nav className='csrNavbar'>
-        <Link to='/' className='logoAndText-flex'>
-          <h2 className='logoText'>
-            Ops<span id='logoText'>pad</span>
+    <div className={s.csrWrapper}>
+      <nav className={s.csrNavbar}>
+        <Link to='/' className={s.logoAndTextFlex}>
+          <h2 className={s.logoText}>
+            Ops<span id={s.logoText}>pad</span>
           </h2>
-          <img src='servers (3) 4.png' alt='logo' className='logo' />
+          <img src='servers (3) 4.png' alt='logo' className={s.logo} />
         </Link>
         <ul>
           <Link to='/about-us'>
@@ -48,15 +48,15 @@ const Csr = () => {
             <li>FAQs</li>
           </Link>
         </ul>
-        <button className='csrBtn'>Download App</button>
+        <button className={s.csrBtn}>Download App</button>
         <img
           src='Vector (13).png'
           alt='menu'
-          className='csrMenuImg'
+          className={s.csrMenuImg}
           onClick={handleNavbar}
         />
         <div
-          className='navbar'
+          className={s.navbar}
           style={isShown ? style : {}}
           onMouseLeave={handleNavbar}
         >
@@ -66,63 +66,63 @@ const Csr = () => {
           <li>FAQs</li>
         </div>
       </nav>
-      <div className='csrMain'>
-        <div className='csrMainSection1'>
-          <p className='csrMainSection1-text'>
+      <div className={s.csrMain}>
+        <div className={s.csrMainSection1}>
+          <p className={s.csrMainSection1Text}>
             OUR CORPORATE SOCIAL RESPONSIBILITY{' '}
           </p>
         </div>
       </div>
-      <div className='csrMainSection2'>
-        <img src='image 9 (2).png' alt='img' className='sectionTwoImg' />
-        <div className='csrMainSections-divs'>
-          <h2 className='csrMainSectionsTitles'>
+      <div className={s.csrMainSection2}>
+        <img src='image 9 (2).png' alt='img' className={s.sectionTwoImg} />
+        <div className={s.csrMainSectionsDivs}>
+          <h2 className={s.csrMainSectionsTitles}>
             Seamless Documentation Process
           </h2>
-          <p className='csrMainSectionDetails'>
+          <p className={s.csrMainSectionDetails}>
             OpsPad is commited to being a responsible solution - we recognise
             the impact of our product and seek to manage them appropriately.
           </p>
         </div>
       </div>
-      <div className='csrMainSection3' id='csrMainSection3'>
+      <div className={s.csrMainSection3} id={s.csrMainSection3}>
         <img
           src='image 8 (1).png'
           alt='img'
-          className='sectionThreeImg'
-          id='sectionThreeImg'
+          className={s.sectionThreeImg}
+          id={s.sectionThreeImg}
         />
-        <div className='csrMainSections-divs'>
-          <h2 className='csrMainSectionsTitles' id='csrMainSectionsTitle'>
+        <div className={s.csrMainSectionsDivs}>
+          <h2 className={s.csrMainSectionsTitles} id={s.csrMainSectionsTitle}>
             Policy Implementation
           </h2>
-          <p className='csrMainSectionDetails'>
+          <p className={s.csrMainSectionDetails}>
             We take all feedback we receive from our users seriously, and ensure
             that we fulfill the requirements while taking the necessary steps to
             realise our corporate responsibilities.
           </p>
         </div>
       </div>
-      <div className='csrMainSection4'>
-        <img src='image 10.png' alt='img' className='sectionFourImg' />
-        <div className='csrMainSections-divs'>
-          <h2 className='csrMainSectionsTitles'>
+      <div className={s.csrMainSection4}>
+        <img src='image 10.png' alt='img' className={s.sectionFourImg} />
+        <div className={s.csrMainSectionsDivs}>
+          <h2 className={s.csrMainSectionsTitles}>
             Support To The Devops Community
           </h2>
-          <p className='csrMainSectionDetails'>
+          <p className={s.csrMainSectionDetails}>
             We aim to demonstrate commitment to the DevOps community through
             actions and within our corporate policies.
           </p>
         </div>
       </div>
-      <div className='csrMainSection5'>
-        <div className='rule'></div>
+      <div className={s.csrMainSection5}>
+        <div className={s.rule}></div>
         <p>
           As a team, we recognize that we must integrate our business values and
           operation to meet the expectation of our user in relation to the
           community and the society at large.
         </p>
-        <button className='csrSectionBtn'>SIGN UP</button>
+        <button className={s.csrSectionBtn}>SIGN UP</button>
       </div>
     </div>
   );
