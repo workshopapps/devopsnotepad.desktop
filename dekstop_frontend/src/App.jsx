e88bcb6d43bb48879c28d61d6f28658d7381cc30
drizzy009 <yusid03@gmail.com>
2022-11-25 14:16:04 +0100

    add: new update

--- dekstop_frontend/src/App.jsx
+++ dekstop_frontend/src/App.jsx
@@ -4,18 +4,19 @@ import {Routes, Route} from 'react-router-dom';
 import Note from './Pages/notes/Note';
 import Home from './Pages/home/Home';
 import Settings from './Pages/Settings/MainSettings';
-// import Sidenav from './Component/SideNav/SideNav';
-import EditProfile from './Pages/Settings/EditProfile/EditProfile';
+import Sidenav from './Component/SideNav/SideNav';
+import Language from './Pages/Settings/components/Languages/Language';
 
 
 function App() {
 	return (
 		<div className="App">
+		<Sidenav/>
 		  <Routes>
 			<Route  path="/" element={<Home />} />
 			<Route exact path="/note" element={<Note/>}/>
 			<Route exact path="/settings" element={<Settings/>}/>
-			<Route exact path="/edit-profile" element={<EditProfile/>}/>
+			<Route exact path="/languages" element={<Language/>}/>
 		  </Routes>
 		</div>
 	);
--- dekstop_frontend/src/Pages/Settings/MainSettings.jsx
+++ dekstop_frontend/src/Pages/Settings/MainSettings.jsx
@@ -27,7 +27,7 @@ function Settings() {
 					<div className={style.account}>
 						<div className={style.accountProfile}>
 							<FaRegUser />
-							<Link to="/">{accounts.func}</Link>
+							<Link to={accounts.slug}>{accounts.func}</Link>
 						</div>
 						<Link to={accounts.slug}>
 							<FaChevronRight />
@@ -69,13 +69,15 @@ function Settings() {
 			{AppSettings.map((appsettings) => (
 				<div key={appsettings.title}>
 					<small className={style.smallHead}>{appsettings.title}</small>
+					<Link to={appsettings.slug}>
 					<div className={style.appSettings_cont}>
 						<div className={style.appSettings}>
 							{appsettings.icon}
-							<Link to={appsettings.slug}>{appsettings.func}</Link>
+							<h3>{appsettings.func}</h3>
 						</div>
 						{appsettings.iconright}
 					</div>
+					</Link>
 					<span className={style.border_bottom}> </span>
 				</div>
 			))}
