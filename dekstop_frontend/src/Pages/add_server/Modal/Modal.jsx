de96997570dd4fda5bd124329f17b446f8f7533b
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-26 08:11:50 +0100

    feat: app updated

--- dekstop_frontend/src/Pages/add_server/Modal/Modal.jsx
+++ dekstop_frontend/src/Pages/add_server/Modal/Modal.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import "./Modal.css"
 import tick from "./tick.png"
 
-const Modal = ({modal, open, close}) => {
+export default function Modal(props){
 
-    if (modal === false){
+    if (props.modal === false){
         return null
     }
 
   return (
-    <div className='modal' onClick={close}>
-        <div className="modal-contain" onClick={(e) => {e.stopPropagation()}}>
+    <div className='modal' onMouseDownCapture={close}>
+        <div className="modal-contain" onMouseDownCapture={(e) => {e.stopPropagation()}}>
             <div className="modal-box">
                 <img src={tick} alt="" className='tick'/>
                 <h4>Server Added Successfully </h4>
                 <p>You have successfully added a <br /> new server </p>
-                <button className='mod-btn'>Back To Dashboard</button>
+                <button type='button' className='mod-btn'>Back To Dashboard</button>
             </div>
         </div>
     </div>
   )
 }
-
-export default Modal
