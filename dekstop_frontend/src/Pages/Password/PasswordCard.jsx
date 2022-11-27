f53ac48d14d777265eb9a013af8af91b4af37a27
heytega <akpovbiyoranthony@gmail.com>
2022-11-27 21:02:12 +0100

    updated functionality for password tab

--- dekstop_frontend/src/Pages/Password/PasswordCard.jsx
+++ dekstop_frontend/src/Pages/Password/PasswordCard.jsx
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CardStyle from './PasswordCard.module.css';
-// import { Box, Stack, Typography, SvgIcon } from '@mui/material';
 import EyeIcon from './images/eye.svg';
 import Dash from './images/stars.svg';
-// import Add from './images/add.svg';
-// import Copy from './images/copy.svg';
-// import Edit from './images/edit.svg';
 import Option from './images/option.svg';
-// import Start from './images/stars.svg';
-// import Warning from './images/warning.svg';
-
-// import VisibilityIcon from '@mui/icons-material/Visibility';
-// import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
-// import { ReactComponent as Dash } from './images/stars.svg';
+import Copy from './images/copy.svg';
+import Edit from './images/edit.svg';
 
 function PasswordCard({ user }) {
 	const [toggleVisibility, setToggleVisibility] = useState(null);
+	const [toggleOptionMenu, setToggleOptionMenu] = useState(false)
+
 	PasswordCard.propTypes = {
 		user: PropTypes.node.isRequired,
 	};
@@ -28,16 +20,6 @@ function PasswordCard({ user }) {
 	}, []);
 
 	return (
-		// <Box
-		// 	sx={{
-		// 		height: '113px',
-		// 		width: '979px',
-		// 		alignContent: 'center',
-		// 		border: '1px solid #C7C7C7',
-		// 		marginBottom: '20px',
-		// 		padding: '20px',
-		// 	}}
-		// >
 		<div className={CardStyle.passwordCard}>
 			<div className={CardStyle.headFlex}>
 				<h1 className={CardStyle.name}>{user.name}</h1>
@@ -55,9 +37,20 @@ function PasswordCard({ user }) {
 					>
 						<img src={EyeIcon} alt="" />
 					</button>
-					<button type="button" className={CardStyle.options}>
+					<button onClick={()=>{setToggleOptionMenu(!toggleOptionMenu)}} type="button" className={CardStyle.options}>
 						<img src={Option} alt="" />
 					</button>
+
+					{toggleOptionMenu && <div className={CardStyle.optionCard}>
+						<button type='button' className={CardStyle.cp}>
+							<img src={Copy} alt="" />{' '}
+							<span className={CardStyle.ct}>Copy Password</span>
+						</button>
+						<button type='button' className={CardStyle.ep}>
+							<img src={Edit} alt="" />
+							<span className={CardStyle.et}>Edit Password</span>
+						</button>
+					</div>}
 				</div>
 			</div>
 			<div className={CardStyle.password}>
