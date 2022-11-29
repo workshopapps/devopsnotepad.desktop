4ffa6ce8cb002f4b412ae1945d6a0b7058e939b3
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-11-29 12:49:51 +0100

    feat(fro-95): Add server context

--- dekstop_frontend/src/Pages/home/Home.jsx
+++ dekstop_frontend/src/Pages/home/Home.jsx
@@ -3,10 +3,15 @@ import Sidenav from '../../Components/SideNav/SideNav';
 import style from './Home.module.css';
 import Onboarding from '../../Components/Onboarding/Onboarding';
 import ServerCard from '../../Components/ServerCard/ServerCard';
-import Servers from './ServerData';
+// import Servers from './ServerData';
+import addBg from './Assets/add.svg';
 
 function Home() {
 	const [newUser, setNewUser] = useState(null);
+	const Servers = [];
+
+	// Initiate Onboarding
+	// Checks local storage if this is the first time the user is using the app, if not a new User, changes new user to true and initiates onboarding process
 	useEffect(() => {
 		const isNewUser = localStorage.getItem('isNewUser');
 		if (!isNewUser) {
@@ -14,11 +19,14 @@ function Home() {
 		}
 	});
 
+	// Disable onboarding for subsequent run
+	// On click get started in onboarding, close onboarding and set isNewUser to false in local storage so that next time the user opens the app it skips the onboarding process
 	const getStarted = useCallback(() => {
 		setNewUser(false);
 		localStorage.setItem('isNewUser', false);
 	}, [newUser]);
 
+	// Query api for user data
 	return (
 		<div className={style.HomeWrapper}>
 			<Sidenav />
@@ -34,6 +42,17 @@ function Home() {
 							serverHealth={server.serverHealth}
 						/>
 					))}
+				</div>
+			)}
+			{Servers.length === 0 && (
+				<div className={style.no_server}>
+					<figure>
+						<img src={addBg} alt="" aria-hidden />
+					</figure>
+					<div className={style.no_server_content}>
+						<h2>Empty Server List</h2>
+						<p>You do not have any servers yet.</p>
+					</div>
 				</div>
 			)}
 		</div>
