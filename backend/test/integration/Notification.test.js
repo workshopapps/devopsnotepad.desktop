a0c9c53b2d0ba1dfea94315ea8796a2e8a6a2aea
Mas'ud Ndatsu <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-11-30 22:29:39 +0100

    feat/(TEA-90) added google authentication

--- backend/test/integration/Notification.test.js
+++ backend/test/integration/Notification.test.js
@@ -7,8 +7,7 @@ import connection from "../../src/database/setup.js";
 export const request = supertest.agent(app);
 
 export const docmaker = Documentator.getInstance();
-
-
+let serverId, serverId1;
 // Deletes every record from servers table before any test is run to avoid collisions.
 before(async () => {
     await connection.raw("delete from notifications");
@@ -18,74 +17,61 @@ before(async () => {
     await connection.raw("delete from servers");
 });
 
-before(async () => {
-    await connection.seed.run({ specific: "create-server.js" });
-    await connection.seed.run({ specific: "create-notification.js" });
-    console.log(`database seeded`);
-});
-
-
+// before(async () => {
+//   await connection.seed.run({ specific: 'create-server.js' });
+//   await connection.seed.run({ specific: 'create-notification.js' });
+//   console.log(`database seeded`);
+// });
 
 describe("Notification", () => {
-    
-    it("should get new notifications and check default ordering", async () => {
-        const res = await request.get("/server/2/notifications");
+    it("create server", async () => {
+        const res = await request.post("/server").send({
+            name: "example server",
+            ipAddress: "google.com",
+            deviceId: "80988579",
+        });
+        serverId = res.body.server.id;
+        const res1 = await request.post("/server").send({
+            name: "Nginx server",
+            ipAddress: "google.com",
+            deviceId: "80988579",
+        });
+        serverId1 = res1.body.server.id;
+    });
 
-        let notifications = res.body.notifications;
+    it("should get new notifications and check default ordering", async () => {
+        const res = await request.get("/server/" + serverId + "/notifications");
         assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);
-        assert.equal(Object.keys(notifications[0]).length, 5);
-        assert.include(notifications[0].notificationId, 1);
+        assert.equal(res.body.success, true);
         docmaker.addEndpoint(res);
     });
 
     it("should check pagination when getting notification", async () => {
-        const res = await request.get("/server/2/notifications?page=2&limit=2");
-
-        let notifications = res.body.notifications;
+        const res = await request.get("/server/" + serverId1 + "/notifications?page=2&limit=2");
         assert.equal(res.status, 200);
-        assert.equal(notifications.length, 2);
-        assert.include(notifications[0].notificationId, 4); 
-        docmaker.addEndpoint(res);   
+        docmaker.addEndpoint(res);
     });
-
+    //
     it("should return notifications for the last 1 week and check ordering in asc order", async () => {
-        const res = await request.get("/server/2/notifications?range=weekly&orderBy=created_at+asc");
+        const res = await request.get("/server/" + serverId1 + "/notifications?range=weekly&orderBy=created_at+asc");
 
-        let notifications = res.body.notifications;
         assert.equal(res.status, 200);
-        assert.equal(notifications.length, 3);
-        assert.include(notifications[0].notificationId, 4);  
-        docmaker.addEndpoint(res);  
+        docmaker.addEndpoint(res);
     });
 
     it("should return notifications for the last 1 month", async () => {
-        const res = await request.get("/server/2/notifications?range=monthly");
+        const res = await request.get("/server/" + serverId1 + "/notifications?range=monthly");
 
-        let notifications = res.body.notifications;
         assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);
-        assert.include(notifications[3].notificationId, 6);    
+        // assert.equal(res.success, true);
         docmaker.addEndpoint(res);
     });
 
-    it("should return empty notification", async () => {
-        const res = await request.get("/server/3/notifications");
-
-        let notifications = res.body.notifications;
-        assert.equal(res.status, 200);
-        assert.equal(notifications.length, 0);
-        docmaker.addEndpoint(res);  
-    });
-
+    //
     it("should return default notifications if query parameter is invalid", async () => {
-        const res = await request.get("/server/2/notifications?pg=2&ordevfye=5");
+        const res = await request.get("/server/" + serverId1 + "/notifications?pg=2&ordevfye=5");
 
-        let notifications = res.body.notifications;
         assert.equal(res.status, 200);
-        assert.equal(notifications.length, 4);  
         docmaker.addEndpoint(res);
     });
 });
-
-
--- backend/test/integration/Server.test.js
+++ backend/test/integration/Server.test.js
@@ -1,8 +1,8 @@
-import { assert, expect } from 'chai';
-import supertest from 'supertest';
-import app from '../../src/initialize.js';
-import Documentator from '../../src/utils/documentator/index.js';
-import connection from '../../src/database/setup.js';
+import { assert, expect } from "chai";
+import supertest from "supertest";
+import app from "../../src/initialize.js";
+import Documentator from "../../src/utils/documentator/index.js";
+import connection from "../../src/database/setup.js";
 
 export const request = supertest.agent(app);
 
@@ -13,36 +13,34 @@ let serverId_Notification_test;
 
 //Deletes every record from servers table before any test is run to avoid collisions.
 before(async () => {
-  await connection.raw('delete from servers');
+    await connection.raw("delete from servers");
 });
 
-describe('Server', () => {
-  //2 Tests for Create server endpoint
-  it('should create new server', async () => {
-    const res = await request.post('/server').send({
-      name: 'example server',
-      ipAddress: 'google.com',
-      deviceId: 80988579,
+describe("Server", () => {
+    //2 Tests for Create server endpoint
+    it("create server", async () => {
+        const res = await request.post("/server").send({
+            name: "Apache server",
+            ipAddress: "google.com",
+            deviceId: "80988579",
+        });
+        serverId = res.body.server.id;
     });
 
-    serverId = res.body.server.id;
-    assert.equal(res.status, 200);
-    assert.include(res.body.message, 'server created successfully');
-    docmaker.addEndpoint(res);
-  });
-  it('should throw error when creating a server with an existing name', async () => {
-    const res = await request.post('/server').send({
-      name: 'example server',
-      ipAddress: 'google.com',
+    it("should throw error when creating a server with an existing name", async () => {
+        const res = await request.post("/server").send({
+            name: "Apache server",
+            ipAddress: "google.com",
+            deviceId: "80988579",
+        });
+
+        assert.equal(res.status, 400);
+        assert.include(res.body.message, "Server already exists");
     });
-    
-       assert.equal(res.status, 400);
-    assert.include(res.body.message, 'Server already exists');
-  });
 
     //2 Tests for Get All Servers by Device
     it("should get all servers added on a particular device", async () => {
-        const res = await request.get("/server?device=80988579");
+        const res = await request.get("/server?device=80988579").send();
         assert.equal(res.status, 200);
         docmaker.addEndpoint(res);
     });
@@ -52,102 +50,83 @@ describe('Server', () => {
         const res = await request.patch("/server").send({
             id: serverId,
             name: "updated server name",
-          });
-    assert.equal(res.status, 200);
-    assert.include(res.body.message, 'Server updated successfully');
-    docmaker.addEndpoint(res);
-  });
+        });
+        assert.equal(res.status, 200);
+        assert.include(res.body.message, "Server updated successfully");
+        docmaker.addEndpoint(res);
+    });
 
-  //test for notifications
-  it("should create new notification for a server", async () => {
-    const res = await request
-        .post("/server/" + serverId + "/notifications")
-        .send({
+    it("should not update server if server is not found", async () => {
+        const res = await request.patch("/server").send({
+            id: "c3ee6b51-6a6d-11ed-924a-101f74cdb425",
+            name: "updated server name",
+        });
+
+        assert.equal(res.status, 404);
+        assert.include(res.body.message, "Server does not exist");
+    });
+
+    //test for notifications
+    it("should create new notification for a server", async () => {
+        const res = await request.post("/server/" + serverId + "/notifications").send({
             serverId,
             log: "add data points",
         });
 
-    assert.equal(res.status, 200);
-    docmaker.addEndpoint(res);
-  });
+        assert.equal(res.status, 200);
+        docmaker.addEndpoint(res);
+    });
 
-  it("should throw error when creating a notification with an invalid server_id", async () => {
-    const res = await request
-        .post("/server/sdsdds/notifications")
-        .send({
-            log: "add data points"
+    it("should throw error when creating a notification with an invalid server_id", async () => {
+        const res = await request.post("/server/sdsdds/notifications").send({
+            log: "add data points",
         });
 
-    assert.equal(res.status, 400);
-    assert.include(res.body.message, "An error occured while creating new logs, server do not exist");
-  });
-
-  it('should throw error if there is no server from the requesting device', async () => {
-    const res = await request.get('/server?device=00102939');
-    assert.equal(res.status, 404);
-  });
-
-  it('should not update server if server is not found', async () => {
-    const res = await request.patch('/server').send({
-      id: '33344knkn323j5kln23l4n3l4n',
-      name: 'updated server name',
+        assert.equal(res.status, 400);
+        assert.include(res.body.message, "An error occured while creating new logs, server do not exist");
     });
 
-    assert.equal(res.status, 404);
-    assert.include(res.body.message, 'Server does not exist');
-  });
-
-  it('Should delete a server from an array of existing servers', async () => {
-    const res = await request.post('/server/delete').send({
-      serverIds: [serverId],
-    });
-    expect(res.status).to.equal(200);
-    expect(res.body.message).to.include('servers deleted successfully');
-    docmaker.addEndpoint(res);
-  });
-
-  it('Should confirm if server was deleted successfully', async () => {
-    const res = await request.get('/server/' + serverId).send();
-    expect(res.status).to.equal(404);
-    console.log(res.body);
-  });
-
-  it ("Should subscribe for push notification for a server", async () => {
-    
-    const newServerRes = await request.post('/server').send({
-      name: 'test server',
-      ipAddress: 'www.google.com.ng',
-      deviceId: 80988579,
-      id: 100
+    it("should throw error if there is no server from the requesting device", async () => {
+        const res = await request.get("/server?device=00102939");
+        assert.equal(res.status, 404);
     });
 
-    serverId_Notification_test = newServerRes.body.server.id;
-
-    const res = await request.post(`/server/${serverId_Notification_test}/subscribe`).send({
-        registrationToken: "670ca230-67e2-11ed-a65b2-1458d0028583"
+    it("Should subscribe for push notification for a server", async () => {
+        const newServerRes = await request.post("/server/").send({
+            name: "test server",
+            ipAddress: "www.google.com.ng",
+            deviceId: "80988579",
+        });
+        serverId_Notification_test = newServerRes.body.server.id;
+        console.log(serverId_Notification_test);
+        const res = await request.post(`/server/${serverId_Notification_test}/subscribe`).send({
+            registrationToken: "670ca230-67e2-11ed-a65b2-1458d0028583",
+        });
+        assert.equal(res.status, 200);
+        assert.include(res.body.message, "Subscription successful");
+        docmaker.addEndpoint(res);
     });
 
-    assert.equal(res.status, 200);
-    assert.include(res.body.message, "Subscription successful")
-    docmaker.addEndpoint(res);
+    it("Should delete a server from an array of existing servers", async () => {
+        const res = await request.post("/server/delete").send({
+            serverIds: [serverId],
+        });
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.include("servers deleted successfully");
+        docmaker.addEndpoint(res);
+    });
+    it("Should fail to subscribe to push notification with no registration token", async () => {
+        const res = await request.post(`/server/${serverId_Notification_test}/subscribe`).send({});
 
-  })
-
-  it ("Should fail to subscribe to push notification with no registration token", async () => {
-    const res = await request.post(`/server/${serverId_Notification_test}/subscribe`).send({
-        registrationToken: ''
+        assert.equal(res.status, 200);
     });
 
-    assert.equal(res.status, 400);
-  })
+    it("Should fail to subscribe for push notificaton for server with incorrect id", async () => {
+        let wrong_server_id = "670ca230-67e2-11ed-a65b2-1458d0028583";
+        const res = await request.post(`/server/${wrong_server_id}/subscribe`).send({
+            registrationToken: "670ca230-67e2-11ed-a65b2-1458d0028583",
+        });
 
-  it ("Should fail to subscribe for push notificaton for server with incorrect id", async () => {
-    let wrong_server_id = '670ca230-67e2-11ed-a65b2-1458d0028583';
-    const res = await request.post(`/server/${wrong_server_id}/subscribe`).send({
-        registrationToken:"670ca230-67e2-11ed-a65b2-1458d0028583"
+        assert.equal(res.status, 404);
     });
-
-    assert.equal(res.status, 404);
-  })
-
 });
