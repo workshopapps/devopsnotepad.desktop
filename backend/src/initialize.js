a62a2d090a78d6e4499f706f6edb35666dc5ac3e
Mas'ud Ndatsu <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-12-01 07:00:39 +0100

    chore bug fixes

--- backend/src/initialize.js
+++ backend/src/initialize.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import passport from "passport";
-import notFoundHandler from "./middleware/application/notFoundHandler.js";
-import errorHandler from "./middleware/application/errorHandler.js";
-import config from "./config/index.js";
-import routes from "./routes/index.js";
-import passportSetup from "./config/passport.js";
-import session from "express-session";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import passport from 'passport';
+import notFoundHandler from './middleware/application/notFoundHandler.js';
+import errorHandler from './middleware/application/errorHandler.js';
+import config from './config/index.js';
+import routes from './routes/index.js';
+import passportSetup from './config/passport.js';
+import session from 'express-session';
 
 const app = express();
 passportSetup();
@@ -17,15 +17,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
-    session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: false,
-    })
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(morgan(config.env.isProduction ? "common" : "dev"));
+app.use(morgan(config.env.isProduction ? 'common' : 'dev'));
 app.use(routes);
 app.use(notFoundHandler);
 app.use(errorHandler);
