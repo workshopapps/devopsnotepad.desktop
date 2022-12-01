c9f4c625e05918fbf6d931f83ea3126b42ff1e0f
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-12-01 19:21:14 +0100

    feat(BE-10): implemented authentication middleware

--- backend/src/middleware/application/authenticate.js
+++ backend/src/middleware/application/authenticate.js
@@ -1,21 +1,32 @@
 import jwt from "jsonwebtoken";
 
 export default async function authenticate(req, res, next) {
-
-    const token = req.cookies.access_token || req.headers.authorization.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).send("No token found");
-    } 
-  
-    jwt.verify(token, process.env.JWT_SECRET, (err, id, email) => {
-        if (err) {
-            res.status(403).send("invalid token");
+    try{
+        if (req.cookies["connect.sid"]) {
+            return next();
         }
-        req.user = {
-            id: id,
-            email: email
-        };
-        next();
-    });
+        let token;
+        if ( req.headers.authorization){
+            token = req.headers.authorization.split(" ")[1];
+        }
+
+        if (!token) {
+            return res.status(401).send("No token found");
+        } 
+  
+        else {
+            jwt.verify(token, process.env.JWT_SECRET, (err, id, email) => {
+                if (err) {
+                    res.status(403).send("invalid token");
+                }
+                req.user = {
+                    id: id,
+                    email: email
+                };
+                next();
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
 }
