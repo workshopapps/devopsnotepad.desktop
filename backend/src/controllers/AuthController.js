eb368980a67ae9ca30e9748bdb2d149cabf71270
Mas'ud Ndatsu <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-12-01 12:57:16 +0100

    chore: Auth failure route added

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -1,20 +1,29 @@
 export default class AuthController {
-    static loginStatus = async (req, res) => {
-        if (req.user) {
-            res.status(200).json({
-                success: true,
-                message: "Login was successful",
-                user: req.user,
-            });
-        } else {
-            res.status(401).json({
-                success: false,
-                message: "Not Authenticated",
-            });
-        }
-    };
-    static logout = async (req, res) => {
-        req.logout();
-        res.redirect("/auth/success");
-    };
+  static loginStatus = async (req, res) => {
+    if (req.user) {
+      res.status(200).json({
+        success: true,
+        message: 'Login was successful',
+        user: req.user,
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: 'Not Authenticated',
+      });
+    }
+  };
+  static loginFailed = async (req, res) => {
+    res.status(401).json({
+      success: false,
+      message: 'Not Authenticated',
+    });
+  };
+  static logout = async (req, res) => {
+    req.logout();
+    res.status(200).json({
+      success: true,
+      message: 'Logout successful',
+    });
+  };
 }
