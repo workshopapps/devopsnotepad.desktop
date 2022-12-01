226f3fcbbdf6bcc15dd69eb597d46265e0a76807
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-12-01 19:29:41 +0100

    feat(BE-10): added authentication middleware to protected route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import serverRoute from './serverRoute.js';
 import notificationRoute from './notificationRoute.js';
 import userRoute from './userRoute.js';
+import authenticate from '../middleware/application/authenticate.js';
+
 
 const router = express.Router();
 
-router.use('/server', serverRoute);
-router.use('/server', notificationRoute);
-router.use("/accounts", userRoute)
+router.use('/server', authenticate, serverRoute);
+router.use('/server', authenticate, notificationRoute);
+router.use("/auth", authenticate, userRoute)
 
 export default router;
