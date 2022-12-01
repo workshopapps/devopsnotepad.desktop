1a39f16a8caec09a467caffdad302c7f61a98bf1
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-12-01 07:02:30 +0100

    chore(BE-10): removed reduntant codes

--- backend/src/database/repositories/NotificationRepo.js
+++ backend/src/database/repositories/NotificationRepo.js
@@ -3,40 +3,26 @@ import APIFeatures from "../../utils/apiFeatures.js";
 
 export default class NotificationRepo {
     static create = async (data) => {
-        return await connection("notifications")
-            .insert(data);
-    };
-
-    static getNotificationById = async (id) => {
-        return connection("notifications").where("notification_id", id).first();
+        return await connection("notifications").insert(data);
     };
 
     static getNotifications = async (id, query) => {
-        const dbQuery = connection("notifications")
-            .where("serverId", id);
-        return new APIFeatures(dbQuery, query)
-            .paginate()
-            .sort()
-            .query; 
+        const dbQuery = connection("notifications").where("serverId", id);
+        return new APIFeatures(dbQuery, query).paginate().sort().query;
     };
 
     static getWeeklyNotifications = async (id, query) => {
         const dbQuery = connection("notifications")
             .where("serverId", id)
             .andWhere(connection.raw(`created_at BETWEEN NOW() - INTERVAL 1 WEEK AND NOW()`));
-        return new APIFeatures(dbQuery, query)
-            .paginate()
-            .sort()
-            .query;
+        return new APIFeatures(dbQuery, query).paginate().sort().query;
     };
 
     static getMonthlyNotifications = async (id, query) => {
         const dbQuery = connection("notifications")
             .where("serverId", id)
             .andWhere(connection.raw(`created_at BETWEEN NOW() - INTERVAL 1 MONTH AND NOW()`));
-        return new APIFeatures(dbQuery, query)
-            .paginate()
-            .sort()
-            .query;
+        return new APIFeatures(dbQuery, query).paginate().sort().query;
     };
 }
+ 
