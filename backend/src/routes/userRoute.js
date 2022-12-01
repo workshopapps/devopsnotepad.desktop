fd0554231c29db971926069d9477594408675913
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-12-01 07:30:08 +0100

    feat(BE-10): implemented logout functunality

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.login);
+router.get('/logout', UserController.logout);
 
 export default router;
