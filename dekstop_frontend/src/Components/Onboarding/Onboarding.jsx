2b61628a52c2524385f750ddee8eee331977607d
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-12-01 12:17:16 +0100

    Update onboarding

--- dekstop_frontend/src/Components/Onboarding/Onboarding.jsx
+++ dekstop_frontend/src/Components/Onboarding/Onboarding.jsx
@@ -1,22 +1,34 @@
 /* eslint-disable react/require-default-props */
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './Onboarding.module.css';
-import screen2 from './Assets/screen2.svg';
+import screen1 from './Assets/screen1.svg';
 
 function Onboarding() {
+	const navigate = useNavigate();
+	// Disable onboarding for subsequent run
+	// On click get started in onboarding, close onboarding and set isNewUser to false in local storage so that next time the user opens the app it skips the onboarding process
+	function onClick() {
+		localStorage.setItem('isNewUser', false);
+		navigate('/');
+	}
+
 	return (
 		<div id="onboarding" className={style.container}>
 			<div className={style.onboarding}>
 				<figure>
-					<img src={screen2} aria-hidden alt="" />
+					<img src={screen1} aria-hidden alt="" />
 				</figure>
 
 				<div className={style.content}>
-					<h1>Welcome to Opspad</h1>
+					<h1>Take Notes, Stay Updated</h1>
 					<p>
 						Join millions of Devops to enjoy a great note taking experience.
 					</p>
 				</div>
+
+				<button onClick={onClick} type="button" className={style.btn}>
+					Get Started
+				</button>
 			</div>
 		</div>
 	);
--- dekstop_frontend/src/Pages/home/Home.jsx
+++ dekstop_frontend/src/Pages/home/Home.jsx
@@ -1,30 +1,27 @@
 import React, { useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import ServerContext from '../../Components/Context/ServerContext';
 import Sidenav from '../../Components/SideNav/SideNav';
 import style from './Home.module.css';
 import ServerCard from '../../Components/ServerCard/ServerCard';
-import Onboarding from '../../Components/Onboarding/Onboarding';
 // import Servers from './ServerData';
 import addBg from './Assets/add.svg';
 
 function Home() {
 	const { servers, loading, getServers } = useContext(ServerContext);
-	// const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	// Initiate Onboarding
 	// Checks local storage if this is the first time the user is using the app, if not a new User, changes new user to true and initiates onboarding process
 	useEffect(() => {
 		getServers();
-		// const isNewUser = localStorage.getItem('isNewUser');
-		// if (!isNewUser) {
-		// 	navigate('/onboarding');
-		// }
+		const isNewUser = localStorage.getItem('isNewUser');
+		if (!isNewUser) {
+			navigate('/onboarding');
+		}
 	});
 
-	return loading ? (
-		<Onboarding />
-	) : (
+	return (
 		<div className={style.HomeWrapper}>
 			<Sidenav />
 			{loading && <div className={style.loading}>Loading Servers...</div>}
