4f2af3c973ecef6ad7f0eb53ae72da61fb3cd2f4
Chima <orlandofays3@gmail.com>
2022-12-01 14:23:17 +0100

    feat:add swagger documentation

---
+++ backend/src/routes/swaggerDocs.js
@@ -0,0 +1,368 @@
+/**
+ * @swagger
+ * /auth/register:
+ *     post:
+ *         summary: Registers a single user
+ *         tags:
+ *             - Register
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                         schema:
+ *                          type: object
+ *                          required:
+ *                              - name
+ *                              - email
+ *                              - password
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  description: The name of the user
+ *                              email:
+ *                                  type: string
+ *                                  description: The email of the user
+ *                              password:
+ *                                  type: string
+ *                                  description: The password of the user
+ *                              created_at:
+ *                                  type: string
+ *                                  description: The time the user was created
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: Email already exists
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: failure message.
+ *             '400':
+ *                 description: Please provide all details
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: failure message.
+ * /auth/login:
+ *     post:
+ *         summary: Logs in a single user with correct login credentials
+ *         tags:
+ *             - Login
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                         schema:
+ *                          type: object
+ *                          required:
+ *                              - email
+ *                              - password
+ *                          properties:
+ *                              email:
+ *                                  type: string
+ *                                  description: The email of the user
+ *                              password:
+ *                                  type: string
+ *                                  description: The password of the user
+ *                              created_at:
+ *                                  type: string
+ *                                  description: The time the user was created
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: Email already exists
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ *             '400':
+ *                 description: Please provide all details
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ * /server:
+ *     patch:
+ *         summary: Updates server information for a single user
+ *         tags:
+ *             - Server
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                         schema:
+ *                          type: object
+ *                          required:
+ *                              - name
+ *                              - ipAddress
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  description: The name of the server
+ *                              ipAddress:
+ *                                  type: string
+ *                                  description: The ipAddress of the server
+ *                              created_at:
+ *                                  type: string
+ *                                  description: The time the user was created
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: Server does not exist
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ * /server/:
+ *     post:
+ *         summary: Creates server for a single user
+ *         tags:
+ *             - Server
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                         schema:
+ *                          type: object
+ *                          required:
+ *                              - name
+ *                              - ipAddress
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  description: The name of the server
+ *                              ipAddress:
+ *                                  type: string
+ *                                  description: The ipAddress of the server
+ *                              created_at:
+ *                                  type: string
+ *                                  description: The time the user was created
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: Server already exists
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ * /server/delete:
+ *     post:
+ *         summary: Deletes selected servers for a single user
+ *         tags:
+ *             - Server
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                         schema:
+ *                          type: object
+ *                          required:
+ *                              - ids
+ *                          properties:
+ *                              name:
+ *                                  type: array
+ *                                  description: The id's of the selected servers
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ * /server?page={page}&limit={limit}:
+ *      get:
+ *         summary: Fetches all servers for a user
+ *         tags:
+ *             - Server
+ *         parameters:
+ *             - in: path
+ *               name: page
+ *               schema:
+ *                    type: number
+ *                    required: true
+ *             - in: path
+ *               name: page
+ *               schema:
+ *                    type: number
+ *                    required: true
+ *         responses:
+ *             '200':
+ *                description: Success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                success:
+ *                                    type: boolean
+ *                                    description: success bolean.
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: There are no servers added on this device
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ *       
+ * /server/{serverId}/notifications:
+ *     post:
+ *         summary: Creates a single notification for a single server
+ *         tags:
+ *             - Server Notification
+ *         parameters:
+ *             - in: path
+ *               name: serverId
+ *               schema:
+ *                    type: string
+ *                    required: true
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: An error occured while creating new logs, server do not exist
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ * /server/{serverId}/notifications/:
+ *     get:
+ *         summary: Fetches all notifications for a single server
+ *         tags:
+ *             - Server Notification
+ *         parameters:
+ *             - in: path
+ *               name: serverId
+ *               schema:
+ *                    type: string
+ *                    required: true
+ * 
+ *         responses:
+ *             '200':
+ *                description: success
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: success message.
+ *                                data:
+ *                                     type: object
+ *             '404':
+ *                 description: An error occured while creating new logs, server do not exist
+ *                 content:
+ *                     application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                message:
+ *                                     type: string
+ *                                     description: fail message.
+ */
