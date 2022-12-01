4c354813490d5fbf6b1123e1e72389ceec8d109a
dread557 <dadaolawunmi09@gmail.com>
2022-12-01 11:51:18 +0100

    Pricing, and our team page done

--- frontend_website/src/Pages/ForgetPassword/Reset.js
+++ frontend_website/src/Pages/ForgetPassword/Reset.js
@@ -4,38 +4,33 @@ import { Link } from "react-router-dom";
 import logo from "../ForgetPassword/Images/logo.png"
 
 
-
-
-
-
 const Reset = () => {
-    return ( 
+    return (
         <div className={forgetStyles.Reset}>
-
-<div className={forgetStyles.logoImg}>
-    <img src={logo} alt="" />
-</div>
+            <div className={forgetStyles.logoImg}>
+                <img src={logo} alt="" />
+            </div>
             <h1>PASSWORD RESET</h1>
-            <p>Enter your Opspad <span>username</span> or the <span>email address</span>  you 
+            <p>Enter your Opspad <span>username</span> or the <span>email address</span>  you
                 used to register.  We’ll send you an email with your username
-                    and a link to reset your password.</p>
+                and a link to reset your password.</p>
 
-                    <div className={forgetStyles.inputEmail}>
-                    
-             <form>
-                <label for="email">Email/Username</label>
-                <input type="text" id="Email" name="email"/>
-                 <Link to='/checkemail' ><input type="submit" value="Submit"/></Link>
-                <Link><input type="cancel" value="Cancel"/></Link>
+            <div className={forgetStyles.inputEmail}>
+
+                <form>
+                    <label for="email">Email/Username</label>
+                    <input type="text" id="Email" name="email" />
+                    <Link to='/checkemail' ><input type="submit" value="Submit" /></Link>
+                    <Link><input type="cancel" value="Cancel" /></Link>
 
                 </form>
-                    </div>
-    
-                    <p>If you still need help, contact <span>Opspad Support</span></p>
+            </div>
+
+            <p>If you still need help, contact <span>Opspad Support</span></p>
         </div>
 
-    
-);
+
+    );
 }
 
 export default Reset;
