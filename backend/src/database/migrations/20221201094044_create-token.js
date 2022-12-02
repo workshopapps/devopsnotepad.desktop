901778a9b2da4854ddc49c163f6959875e0dbc9f
Mofe <moflux620@gmail.com>
2022-12-02 09:16:17 +0100

    FEAT: BAC-9 Forgot Password and Password Reset Website

--- backend/src/database/migrations/20221201094044_create-token.js
+++ backend/src/database/migrations/20221201094044_create-token.js
@@ -1,9 +1,10 @@
 export async function up(knex) {
     return knex.schema.createTable("tokens", (table) => {
-        table.increments("id").primary();
+        table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
         table.string("userId", 255).notNullable();
         table.string("token", 255).notNullable();
         table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("expires_at").defaultTo(knex.raw("SELECT date_add(?, INTERVAL ? hour)", [knex.fn.now(), 1]));
     });
 }
   
