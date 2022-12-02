89c1257ae54e83e283160294304c438582c2f739
drizzy009 <yusid03@gmail.com>
2022-12-03 00:28:22 +0100

    add:Implement Create password functionality on the Onboarding Page

--- dekstop_frontend/src/Components/Onboarding/Onboarding.jsx
+++ dekstop_frontend/src/Components/Onboarding/Onboarding.jsx
@@ -1,28 +1,18 @@
 /* eslint-disable react/require-default-props */
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import style from './Onboarding.module.css';
 import screen1 from './Assets/screen1.svg';
-import GlobalPassword from '../GlobalPassword/CreatePassword';
+import CreatePassword from '../GlobalPassword/CreatePassword';
 
-function Onboarding({ closeOnboarding }) {
-	
+function Onboarding() {
 	const [getStarted, setGetStarted] = useState(1);
-	const navigate = useNavigate();
-	localStorage.setItem('isNewUser', false);
-		navigate('/');
+	
 	// Disable onboarding for subsequent run
 	// On click get started in onboarding, close onboarding and set isNewUser to false in local storage so that next time the user opens the app it skips the onboarding process
-	
 
 	return (
 		<div id="onboarding" className={style.container}>
 			{getStarted === 1 ? (
-				<div>
-					<GlobalPassword onClick={closeOnboarding} />
-				</div>
-			) : (
 				<div className={style.onboarding}>
 					<figure>
 						<img src={screen1} aria-hidden alt="" />
@@ -31,20 +21,33 @@ function Onboarding({ closeOnboarding }) {
 					<div className={style.content}>
 						<h1>Take Notes, Stay Updated</h1>
 						<p>
-							Join millions of Devops to enjoy a great note taking experience.
+							Enjoy a great note taking experience while ensuring that servers
+							monitored are up to date.
 						</p>
 					</div>
 
-					<button onClick={() => setGetStarted(2)} type="button" className={style.btn}>
-						Get Started
-					</button>
+					<div className={style.action}>
+						<div className={style.ellipse_container}>
+							<div className={`${style.ellipse} ${style.ellipse_current}`} />
+							<div className={style.ellipse} />
+						</div>
+
+						<button
+							onClick={() => setGetStarted(2)}
+							type="button"
+							className={style.btn}
+						>
+							Next
+						</button>
+					</div>
+				</div>
+			) : (
+				<div className={style.onboarding_form}>
+					<CreatePassword/>
 				</div>
 			)}
 		</div>
 	);
 }
-Onboarding.propTypes = {
-	closeOnboarding: PropTypes.func,
-};
 
 export default Onboarding;
