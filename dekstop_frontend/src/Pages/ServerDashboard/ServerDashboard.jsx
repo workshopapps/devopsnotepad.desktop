4b0b91011d952f9a8faeea2f583009e5696f9a0a
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-12-02 14:24:25 +0100

    fix issue with blank page on reload

--- dekstop_frontend/src/Pages/ServerDashboard/ServerDashboard.jsx
+++ dekstop_frontend/src/Pages/ServerDashboard/ServerDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams, Outlet } from 'react-router-dom';
 import ServerContext from '../../Components/Context/ServerContext';
 import ServerInfo from '../../Components/ServerInfo/ServerInfo';
@@ -6,42 +6,41 @@ import Sidenav from '../../Components/SideNav/SideNav';
 import styles from './ServerDashBoard.module.css';
 
 function ServerDashBoard() {
-	const { getServers, servers } = useContext(ServerContext);
+	const { servers } = useContext(ServerContext);
 	const params = useParams();
 
-	useEffect(() => {
-		getServers();
-	}, [servers]);
-
-	const currentServer = servers.filter(
-		(server) => server.serverId === params.serverId
-	);
-	// const { deviceId, id, ipAddress, name, notification, updated_at } =
-	// 	currentServer[0];
-	const { id, ipAddress, name } = currentServer[0];
 	return (
 		<div>
 			<Sidenav />
 			<section className={styles.main}>
-				<ServerInfo key={id} ipAddress={ipAddress} name={name} />
-
-				<div className={styles.wrapper}>
-					<Link to={`/server/${id}/note`}>
-						{' '}
-						<p className={styles.note}>Notes</p>{' '}
-					</Link>
-					<Link to={`/server/${id}/password`}>
-						{' '}
-						<p className={styles.note}>Password</p>{' '}
-					</Link>
-					<Link to={`/server/${id}/notification`}>
-						{' '}
-						<p className={styles.note}>Notifications</p>{' '}
-					</Link>
-				</div>
+				{servers
+					.filter((server) => server.id === params.id)
+					.map((server) => (
+						<div>
+							<ServerInfo
+								key={server.serverId}
+								ipAddress={server.ipAddress}
+								name={server.name}
+							/>
+							<div className={styles.wrapper}>
+								<Link to={`/server/${server.id}/note`}>
+									{' '}
+									<p className={styles.note}>Notes</p>{' '}
+								</Link>
+								<Link to={`/server/${server.id}/password`}>
+									{' '}
+									<p className={styles.note}>Password</p>{' '}
+								</Link>
+								<Link to={`/server/${server.id}/notification`}>
+									{' '}
+									<p className={styles.note}>Notifications</p>{' '}
+								</Link>
+							</div>
+							<Outlet context={[server]} />
+						</div>
+					))}
 
 				{/* Outlet */}
-				<Outlet context={[currentServer[0]]} />
 			</section>
 		</div>
 	);
