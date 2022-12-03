eb3807f8f246c61e21a93346e685690cf0cfd56f
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2022-12-03 09:04:43 +0000

    implement change password from mobile

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -2,157 +2,185 @@ import create from "../services/user/create.js";
 import UserRepo from "../database/repositories/UserRepo.js";
 import login from "../services/user/login.js";
 import ResetTokenRepo from "../database/repositories/ResetTokenRepo.js";
-import {NotFoundError} from "../lib/errors/index.js";
+import { NotFoundError } from "../lib/errors/index.js";
 import { sendResetLink } from "../services/user/password_reset.js";
 import EmailVerificationTokenRepo from "../database/repositories/emailVerificationRepo.js";
 import bcrypt from "bcrypt";
 
 export default class AuthController {
-    static signup = async (req, res, next) => {
-        try {
-            await create(req.body);
+  static signup = async (req, res, next) => {
+    try {
+      await create(req.body);
 
-            res.status(201).json({
-                success: true,
-                message: "signup successful",
-                user: req.body
-            });
-        } catch (error) {
-            next(error);
-        }
-    };
+      res.status(201).json({
+        success: true,
+        message: 'signup successful',
+        user: req.body,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 
-    static loginUser = async (req, res, next) => {
-        const body = req.body;
-        try {
-            const loggedInUser = await login(body, req, res);
-   
-            //set request cookie
-            if (loggedInUser.user && loggedInUser.token){
-                req.session.user = loggedInUser.user;
-                req.session.authorized = true;
-  
-                return res.send({ 
-                    message: "Logged in Successfully",
-                    user: loggedInUser.user,
-                    token: loggedInUser.token,
-                });  
-            }                   
-        } catch (error) {
-            next(error);
-        }
-    };
+  static loginUser = async (req, res, next) => {
+    const body = req.body;
+    try {
+      const loggedInUser = await login(body, req, res);
 
-    static logoutUser = async (req, res, next) => {
-        try {
-            res.clearCookie("connect.sid");
-            return res.send({
-                message: "logout successful"
-            });
-        } catch (error) {
-            next(error);
-        }
-    };
-    
-    static loginStatus = async (req, res) => {
-        if (req.user) {
-            res.status(200).json({
-                success: true,
-                message: "Login was successful",
-                user: req.user,
-            });
-        } else {
-            res.status(401).json({
-                success: false,
-                message: "Not Authenticated",
-            });
-        }
-    };
-    static loginFailed = async (req, res) => {
-        res.status(401).json({
-            success: false,
-            message: "Not Authenticated",
+      //set request cookie
+      if (loggedInUser.user && loggedInUser.token) {
+        req.session.user = loggedInUser.user;
+        req.session.authorized = true;
+
+        return res.send({
+          message: 'Logged in Successfully',
+          user: loggedInUser.user,
+          token: loggedInUser.token,
         });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  static logoutUser = async (req, res, next) => {
+    try {
+      res.clearCookie('connect.sid');
+      return res.send({
+        message: 'logout successful',
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  static loginStatus = async (req, res) => {
+    if (req.user) {
+      res.status(200).json({
+        success: true,
+        message: 'Login was successful',
+        user: req.user,
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: 'Not Authenticated',
+      });
+    }
+  };
+  static loginFailed = async (req, res) => {
+    res.status(401).json({
+      success: false,
+      message: 'Not Authenticated',
+    });
+  };
+  static logout = async (req, res) => {
+    req.logout();
+    res.status(200).json({
+      success: true,
+      message: 'Logout successful',
+    });
+  };
+
+  static getResetLink = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+      const registeredUser = await UserRepo.getUserByEmail(email);
+
+      if (!registeredUser) throw new NotFoundError('Please input a valid registered email.');
+
+      const { name, id } = registeredUser;
+
+      sendResetLink(email, name, id);
+
+      res.status(200).redirect('/login');
+      return { message: 'A password reset link has been sent to your email address' };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  static updateUserPassword = async (req, res) => {
+    const { token, id, password } = req.body;
+
+    await ResetTokenRepo.deleteExpiredTokens();
+
+    const storedToken = await ResetTokenRepo.getToken(token, id);
+
+    if (!storedToken) {
+      throw new Error('Invalid or expired password reset token');
+    }
+
+    const isValid = await bcrypt.compare(token, storedToken.token);
+
+    if (!isValid) {
+      throw new Error('Invalid or expired password reset token');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, Number(10));
+
+    const updatedUser = await UserRepo.updatePasswordById(id, hashedPassword);
+
+    if (!updatedUser) throw new NotFoundError('User not found');
+
+    res.status(200).redirect('/login');
+    return {
+      success: true,
+      message: 'Password has been updated successfully',
+      updatedUser,
     };
-    static logout = async (req, res) => {
-        req.logout();
-        res.status(200).json({
-            success: true,
-            message: "Logout successful",
-        });
-    };
+  };
 
-    static getResetLink = async (req, res) => {
-        const {email} = req.body;
+  static updateUserPasswordFromMobile = async (req, res) => {
 
-        try {
-            const registeredUser = await UserRepo.getUserByEmail(email);
+    try {
+      // destruct request body
+      const { id, oldPassword, newPassword } = req.body;
 
-            if (!registeredUser) throw new NotFoundError("Please input a valid registered email.");
+      // Get user from database
+      const user = await UserRepo.getUserById(id);
 
-            const {name, id} = registeredUser;
+      // Throw error if user not found
+      if (!user) {
+        return res.status(400).send('User Not Found');
+      }
 
-            sendResetLink(email, name, id);
+      // Compare Old Password with new Password
+      if (oldPassword !== user.password) {
+        return res.status(400).send('User Passwords do not match');
+      }
 
-            res.status(200).redirect("/login");
-            return ({message: "A password reset link has been sent to your email address"});
-        
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      // Save new password
+      await UserRepo.updatePasswordById(id, newPassword);
+      // Success
+      return res.status(201).send('Password Successfully Updated');
+    } catch (error) {
+      return res.status(500).send(`An error occurred whiles updating user password ${error}`)
+    }
 
-    static updateUserPassword = async (req, res) => {
-        const {token, id, password} = req.body;
-        
-        await ResetTokenRepo.deleteExpiredTokens();
 
-        const storedToken = await ResetTokenRepo.getToken(token, id);
+  }
 
-        if (!storedToken) {
-            throw new Error("Invalid or expired password reset token");
-        }
-        
-        const isValid = await bcrypt.compare(token, storedToken.token);
-        
-        if (!isValid) {
-            throw new Error("Invalid or expired password reset token");
-        }
+  static verifyEmail = async (req, res) => {
+    const { token, id } = req.query;
 
-        const hashedPassword = await bcrypt.hash(password, Number(10));
+    await EmailVerificationTokenRepo.deleteExpiredTokens();
 
-        const updatedUser = await UserRepo.updatePasswordById(id, hashedPassword);
+    const storedToken = await EmailVerificationTokenRepo.getToken(id);
 
-        if(!updatedUser)throw new NotFoundError("User not found");
+    if (!storedToken) {
+      throw new Error('Invalid or expired email verification token');
+    }
 
-        res.status(200).redirect("/login");
-        return ({   
-            success: true,
-            message: "Password has been updated successfully",
-            updatedUser
-        });
-    };
+    const isValid = await bcrypt.compare(token, storedToken.token);
 
-    static verifyEmail = async (req, res) => {
-        const { token, id } = req.query;
+    if (!isValid) {
+      throw new Error('Invalid or expired email verification token');
+    }
 
-        await EmailVerificationTokenRepo.deleteExpiredTokens();
+    await UserRepo.updateById(id, { email_verified: 'true' });
 
-        const storedToken = await EmailVerificationTokenRepo.getToken(id);
-
-        if (!storedToken) {
-            throw new Error("Invalid or expired email verification token");
-        }
-    
-        const isValid = await bcrypt.compare(token, storedToken.token);
-    
-        if (!isValid) {
-            throw new Error("Invalid or expired email verification token");
-        }
-
-        await UserRepo.updateById(id, {email_verified: "true"});
-
-        return ({message: "email verified successfully"});
-    };
-  
+    return { message: 'email verified successfully' };
+  };
 }
