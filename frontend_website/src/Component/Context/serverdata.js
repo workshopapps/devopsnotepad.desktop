4250e4b6760ac8fc90db9b218e0d8bba1c7f930c
Joel Ojerinde <joelojerinde@gmail.com>
2022-12-05 14:35:23 +0100

    feat: create server UI and API integration

---
+++ frontend_website/src/Component/Context/serverdata.js
@@ -0,0 +1,31 @@
+const serverData = [
+	{
+		id: '86329fe-6cb2-11ed-bd06-fc3fdbff49c4',
+		name: 'example server',
+		notification: '86329f59-6cb2-11ed-bd06-fc3fdbff49c4',
+		ipAddress: 'google.com',
+		deviceId: '80988579',
+		created_at: '2022-11-25T11:15:52.000Z',
+		updated_at: '2022-11-25T11:15:52.000Z',
+	},
+	{
+		id: '8810f376-6cb2-11ed-bd6-fc3fdbff49c3',
+		name: 'example server 2',
+		notification: '8810f395-6cb2-11ed-bd06-fc3fdbff49c4',
+		ipAddress: 'yahoo.com',
+		deviceId: '80988579',
+		created_at: '2022-11-25T11:15:56.000Z',
+		updated_at: '2022-11-25T11:15:56.000Z',
+	},
+	{
+		id: '8810f376-6cb2-11ed-bd06-fc3fdbff9c1',
+		name: 'example server 3',
+		notification: '8810f395-6cb2-11ed-bd06-fc3fdbff49c4',
+		ipAddress: 'mail.com',
+		deviceId: '80988579',
+		created_at: '2022-11-25T11:15:56.000Z',
+		updated_at: '2022-11-25T11:15:56.000Z',
+	},
+];
+
+export default serverData;
--- frontend_website/src/Pages/Prices/Prices.js
+++ frontend_website/src/Pages/Prices/Prices.js
@@ -6,6 +6,8 @@ import PriceCard from './components/PriceCard/PriceCard';
 import bad from './images/x.svg';
 import good from './images/good.svg';
 import SingleGridTable from './components/SingleGridTable/SingleGridTable';
+import Navigation from '../../Component/Navigation/Navigation';
+import Footer from '../../Component/Footer/Footer';
 
 const Prices = () => {
   const [toggleMonthly, setToggleMonthly] = useState(true);
@@ -15,160 +17,162 @@ const Prices = () => {
   };
 
   return (
-    <section className={style.pricesContainer}>
-      <div className={style.pricesHeaderContainer}>
-        <h1 className={style.pricesHeader}>
-          Opspad has{' '}
-          <span className={style.pricesHeaderBlue}>simple pricing</span> for
-          everyone
-        </h1>
-        <h3 className={style.pricesSubheader}>Choose Your Plan</h3>
-        <div className={style.toggleComment}>
-          <ToggleSwitch
-            toggleMonthly={toggleMonthly}
-            handleToggle={handleToggle}
-          />
-        </div>
-      </div>
-      <div className={style.bgPriceGrid}>
-        <div className={style.pricesSampleGrid}>
-          {!toggleMonthly
-            ? Data[0].items.map((item, index) => {
-              return (
-                <PriceCard
-                  toggleMonthly={toggleMonthly}
-                  card={item}
-                  id={index}
-                />
-              );
-            })
-            : Data[1].items.map((card, index) => {
-              return (
-                <PriceCard
-                  toggleMonthly={toggleMonthly}
-                  card={card}
-                  id={index}
-                />
-              );
-            })}
-        </div>
-      </div>
-      <div className={style.comparePrice}>
-        <h1 className={style.comparePriceHeader}>
-          Compare Our Plans
-        </h1>
-        <div className={`${style.tableContainer} ${style.hidden}`}>
-          <ul className={style.ul}>
-            <li className={`${style.li} ${style.starter}`}>Starter</li>
-            <li className={style.li}>Professional</li>
-            <li className={style.li}>Organization</li>
-          </ul>
-
-          {/* FOR DESKTOP SCREENS */}
-          <div className={style.gridTable}>
-            <h9 className={style.gridHeader}>Unlimited Number of servers</h9>
-            <img
-              className={`${style.gridItem} ${style.bad}`}
-              src={bad}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <h9 className={style.gridHeader}>Notes</h9>
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <h9 className={style.gridHeader}>Third party integration</h9>
-            <img
-              className={`${style.gridItem} ${style.bad}`}
-              src={bad}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <h9 className={style.gridHeader}>Collaborators</h9>
-            <img
-              className={`${style.gridItem} ${style.bad}`}
-              src={bad}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.bad}`}
-              src={bad}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <h9 className={style.gridHeader}>Unlimited downloads</h9>
-            <img
-              className={`${style.gridItem} ${style.bad}`}
-              src={bad}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <h9 className={style.gridHeader}>Access to Opspad community</h9>
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
-            />
-            <img
-              className={`${style.gridItem} ${style.good}`}
-              src={good}
-              alt=''
+    <>
+      <Navigation />
+      <section className={style.pricesContainer}>
+        <div className={style.pricesHeaderContainer}>
+          <h1 className={style.pricesHeader}>
+            Opspad has{' '}
+            <span className={style.pricesHeaderBlue}>simple pricing</span> for
+            everyone
+          </h1>
+          <h3 className={style.pricesSubheader}>Choose Your Plan</h3>
+          <div className={style.toggleComment}>
+            <ToggleSwitch
+              toggleMonthly={toggleMonthly}
+              handleToggle={handleToggle}
             />
           </div>
-
-          {/* /FOR MOBILE / TABLET SCREEN */}
         </div>
-        <SingleGridTable />
-      </div>
-    </section>
+        <div className={style.bgPriceGrid}>
+          <div className={style.pricesSampleGrid}>
+            {!toggleMonthly
+              ? Data[0].items.map((item, index) => {
+                  return (
+                    <PriceCard
+                      toggleMonthly={toggleMonthly}
+                      card={item}
+                      id={index}
+                    />
+                  );
+                })
+              : Data[1].items.map((card, index) => {
+                  return (
+                    <PriceCard
+                      toggleMonthly={toggleMonthly}
+                      card={card}
+                      id={index}
+                    />
+                  );
+                })}
+          </div>
+        </div>
+        <div className={style.comparePrice}>
+          <h1 className={style.comparePriceHeader}>Compare Our Plans</h1>
+          <div className={`${style.tableContainer} ${style.hidden}`}>
+            <ul className={style.ul}>
+              <li className={`${style.li} ${style.starter}`}>Starter</li>
+              <li className={style.li}>Professional</li>
+              <li className={style.li}>Organization</li>
+            </ul>
+
+            {/* FOR DESKTOP SCREENS */}
+            <div className={style.gridTable}>
+              <h9 className={style.gridHeader}>Unlimited Number of servers</h9>
+              <img
+                className={`${style.gridItem} ${style.bad}`}
+                src={bad}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <h9 className={style.gridHeader}>Notes</h9>
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <h9 className={style.gridHeader}>Third party integration</h9>
+              <img
+                className={`${style.gridItem} ${style.bad}`}
+                src={bad}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <h9 className={style.gridHeader}>Collaborators</h9>
+              <img
+                className={`${style.gridItem} ${style.bad}`}
+                src={bad}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.bad}`}
+                src={bad}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <h9 className={style.gridHeader}>Unlimited downloads</h9>
+              <img
+                className={`${style.gridItem} ${style.bad}`}
+                src={bad}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <h9 className={style.gridHeader}>Access to Opspad community</h9>
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+              <img
+                className={`${style.gridItem} ${style.good}`}
+                src={good}
+                alt=''
+              />
+            </div>
+
+            {/* /FOR MOBILE / TABLET SCREEN */}
+          </div>
+          <SingleGridTable />
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
