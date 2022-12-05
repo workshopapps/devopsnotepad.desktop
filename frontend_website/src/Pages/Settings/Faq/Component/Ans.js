e88201ec2ec269bf8e8fe8fe6c2defe433983a3e
Vctcode <victorode01@gmail.com>
2022-12-05 15:11:29 +0100

    Feat(FRO-35): Setting page UI and integration

---
+++ frontend_website/src/Pages/Settings/Faq/Component/Ans.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+const Ans = ({answer}) => {
+    return(
+        <div>
+            {answer}             
+        </div>
+    )
+}
+
+export default Ans;
