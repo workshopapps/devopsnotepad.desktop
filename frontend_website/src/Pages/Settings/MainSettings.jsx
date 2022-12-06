e134fe6a17265dbabd8fade06fe05d134cdf1c3a
Vctcode <victorode01@gmail.com>
2022-12-06 14:56:14 +0100

    Feat(FRO-35): Setting page UI and integration

--- frontend_website/src/Pages/Settings/MainSettings.jsx
+++ frontend_website/src/Pages/Settings/MainSettings.jsx
@@ -1,51 +1,74 @@
 /* eslint-disable no-lone-blocks */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import { Support } from './data';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import style from './MainSettings.module.css';
 import Sidenav from '../../Component/SideNav/SideNav';
 // import ChangePassword from '../../Component/GlobalPassword/ChangePassword';
 import styles from '../../Component/GlobalPassword/ChangePassword.module.css';
 
-function Settings(changed) {
-	const [editpassword, setEditPassword] = useState('');
-	const [confirmpassword, setConfirmPassword] = useState('');
+
+
+function Settings() {
+	const [oldPassword, setOldPassword] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
 	const navigate = useNavigate();
 
-	
+
 	// Proptype declaration
 	// Settings.propTypes = {
 	// 	changed: PropTypes.node.isRequired,
 	// };
 	function isAuthenticated() {
-		
+		const user = JSON.parse(localStorage.getItem("loggedInUser"));
+		return user;
 	}
+
+	// const API_URL = "https://opspad.hng.tech/auth/update-password";
+	const API_URL = "https://opspad.hng.tech/api/auth/update-user-password";
+	
+
+	const user = isAuthenticated();
+	// console.log(user)
+	const email = user.user.email;
+	const username = user.user.name;
 
 	async function changePassword(e) {
 		e.preventDefault();
-		
-		const API_URL = "https://opspad.onrender.com/auth/update-password";
-		const data = {
-			password: editpassword,
-			id: "userId",
-			token: "token"
-		};
-		
-		if (editpassword === confirmpassword) {
-			const res = await axios.post( API_URL, data)
+		console.log("Button is working")
 
+		try {
+			console.log("Try block is working")
+			// const data = {
+			// 	password: editpassword,
+			// 	id: user.user.id,
+			// 	token: user.token
+			// };
+			// console.log(data)
+
+			await axios.post(API_URL, {
+				oldPassword: oldPassword,
+				newPassword: newPassword,
+				// confirmPassword: confirmPassword
+			  })
+				
+				.then((response) => {
+					setNewPassword(response.data);
+				});
+
+			console.log("This is working")
 			alert('Password changed is  Successful');
 			navigate('/');
+		} catch (error) {
+			console.log(error.message)
+
 		}
-		if (editpassword !== confirmpassword) {
-			alert('New Password does not match');
-		}
-		setEditPassword('');
+		setNewPassword('');
 	}
 
-	const username = "Susan";
 
 	return (
 		<div>
@@ -65,7 +88,7 @@ function Settings(changed) {
 
 								<br />
 								<label>Existing mail</label>
-								<p>existingmail@gmail.com</p>
+								<p>{email}</p>
 								<br />
 								<label htmlFor="username">Username</label>
 								<input
@@ -84,9 +107,9 @@ function Settings(changed) {
 									name="oldpass"
 									placeholder=""
 									className={styles.change_input}
-									value={editpassword.oldpass}
+									value={oldPassword.oldpass}
 									onChange={(e) => {
-										setEditPassword(e.target.value);
+										setOldPassword(e.target.value);
 									}}
 									required
 								/>
@@ -96,9 +119,9 @@ function Settings(changed) {
 									type="password"
 									name="newpass"
 									className={styles.change_input}
-									value={editpassword.newpass}
+									value={newPassword.newpass}
 									onChange={(e) => {
-										setEditPassword(e.target.value);
+										setNewPassword(e.target.value);
 									}}
 									required
 								/>
@@ -108,15 +131,15 @@ function Settings(changed) {
 									type="password"
 									name="cofirmNewPass"
 									className={styles.change_input}
-									value={confirmpassword}
+									value={confirmPassword}
 									onChange={(e) => {
 										setConfirmPassword(e.target.value);
 									}}
 									required
 								/>
 
-								{editpassword === confirmpassword ? <button onClick={changePassword}  >Update Changes</button> : <>
-									<button disabled style={{ opacity: 0.86 }}>Update Changes</button>
+								{newPassword === confirmPassword ? <button onClick={changePassword}  >Update Changes</button> : <>
+									<button disabled style={{ opacity: 0.5 }}>Update Changes</button>
 								</>
 
 								}
