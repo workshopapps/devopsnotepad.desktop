a05d021f19704cc24ec2f945650dab456bc11fbe
Usenmfon <usenmfonuko@gmail.com>
2022-12-10 10:13:58 +0100

    chore: updated swagger doc to reflect new endpoint

--- backend/src/services/contactUs/create.js
+++ backend/src/services/contactUs/create.js
@@ -1,13 +1,7 @@
 import ContactRepo from "../../database/repositories/contactUsRepo.js";
-// import { ValidationError } from "../../lib/errors";
 
 export default async function create(body){
-
-    // const error = await 
-
     await ContactRepo.create(body);
 
-    return {
-        message: "successfully sent"
-    };
+    return { message: "successfully sent" };
 }
