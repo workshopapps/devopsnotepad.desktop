22b80ebe00848dd124db5499c2f74d8ff4fc479b
Usenmfon <usenmfonuko@gmail.com>
2022-12-10 07:31:06 +0100

    completed implementation

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import serverRoute from './serverRoute.js';
-import notificationRoute from './notificationRoute.js';
-import userRoute from './userRoute.js';
+import express from "express";
+import serverRoute from "./serverRoute.js";
+import notificationRoute from "./notificationRoute.js";
+import userRoute from "./userRoute.js";
+import contactUsRoute from "./contactUsRoute.js";
 
 const router = express.Router();
 
-router.use('/auth', userRoute);
-router.use('/server', serverRoute);
-router.use('/server', notificationRoute);
+router.use("/auth", userRoute);
+router.use("/server", serverRoute);
+router.use("/server", notificationRoute);
+router.use("/contact-us", contactUsRoute);
 
 export default router;
