62a21cdc7da2bf894049e08c964832ad5f9dbd3c
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2022-12-10 13:59:43 +0000

    notify-me

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import serverRoute from "./serverRoute.js";
 import notificationRoute from "./notificationRoute.js";
 import userRoute from "./userRoute.js";
 import contactUsRoute from "./contactUsRoute.js";
+import notifyMeRoute from "./notifyMeRoute.js"
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", userRoute);
 router.use("/server", serverRoute);
 router.use("/server", notificationRoute);
 router.use("/contact-us", contactUsRoute);
+router.use("/notify-me", notifyMeRoute);
 
 export default router;
