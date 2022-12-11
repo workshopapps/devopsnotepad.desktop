b019d1fe69907ffe776ea1b7152c8134f395ad98
Vctcode <victorode01@gmail.com>
2022-12-11 09:17:34 +0100

    Style: Password visibility for change password in settings

--- frontend_website/src/Pages/Settings/MainSettings.jsx
+++ frontend_website/src/Pages/Settings/MainSettings.jsx
@@ -7,11 +7,17 @@ import { ValidatePassword } from '../SignUp/lib';
 import style from './MainSettings.module.css';
 import Sidenav from '../../Component/SideNav/SideNav';
 import styles from '../../Component/GlobalPassword/ChangePassword.module.css';
+import { AiFillEyeInvisible } from 'react-icons/ai'
+import { AiFillEye } from 'react-icons/ai';
 
 
 
 function Settings() {
-	const [confirmPassword, setConfirmPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
+	const [showNewPassword, setShowNewPassword] = useState(false);
+	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+	// const [passwordIcon] = useState(true);
+	// const [confirmPassword, setConfirmPassword] = useState('');
 	const navigate = useNavigate();
 	const [form, setForm] = useState({
 		oldPassword: '',
@@ -20,16 +26,18 @@ function Settings() {
 		newPassword: '',
 		newPasswordIsValid: false,
 		newPasswordIsFocus: false,
+		confirmPassword: '',
+		confirmPasswordIsValid: false,
+		confirmPasswordIsFocus: false,
 	});
 
-	
-	
+
+	// For Old Password
 	const oldPasswordOnChangeHandler = (e) => {
 		setForm((prev) => {
-		  return { ...prev, oldPassword: e.target.value };
+			return { ...prev, oldPassword: e.target.value };
 		});
 	};
-
 	const oldPasswordOnBlurHandler = (e) => {
 		setForm((prev) => {
 			return { ...prev, oldPasswordIsFocus: true };
@@ -47,10 +55,10 @@ function Settings() {
 		}
 	};
 
-
+	// For New Password
 	const newPasswordOnChangeHandler = (e) => {
 		setForm((prev) => {
-		  return { ...prev, newPassword: e.target.value };
+			return { ...prev, newPassword: e.target.value };
 		});
 	};
 
@@ -71,6 +79,30 @@ function Settings() {
 		}
 	};
 
+	// For New Password
+	const confirmPasswordOnChangeHandler = (e) => {
+		setForm((prev) => {
+			return { ...prev, confirmPassword: e.target.value };
+		});
+	};
+
+	const confirmPasswordOnBlurHandler = (e) => {
+		setForm((prev) => {
+			return { ...prev, confirmPasswordIsFocus: true };
+		});
+
+		const isValid = ValidatePassword(form.confirmPassword);
+		if (isValid) {
+			setForm((prev) => {
+				return { ...prev, confirmPasswordIsValid: true };
+			});
+		} else {
+			setForm((prev) => {
+				return { ...prev, confirmPasswordIsValid: false };
+			});
+		}
+	};
+
 
 
 
@@ -86,6 +118,7 @@ function Settings() {
 	const username = user.user.name;
 	const token = user.token;
 
+	// Submit handler function
 	async function changePassword(e) {
 		e.preventDefault();
 		console.log("Button is working")
@@ -141,56 +174,78 @@ function Settings() {
 
 							<div>
 								<label htmlFor="current">Old Password</label>
-								<input
-									type="password"
-									placeholder="MinLength(8), uppercase, lowercase, character, number"
-									className={styles.change_input}
-									value={form.oldPassword}
-									onChange={oldPasswordOnChangeHandler}
-									invalid={!form.oldPasswordIsValid && form.oldPasswordIsFocus ? 'invalid' : ''}
-									onBlur={oldPasswordOnBlurHandler}
-									required
-								/>
-								{form.oldPasswordIsFocus && !form.oldPasswordIsValid && (
-									<pre className={styles.invalid__input}>
-										MinLength(8), uppercase, lowercase, character, number
-									</pre>
-								)}
-								<label htmlFor="newpass">New Password</label>
+								<div className={styles.input__cont}>
+									<input
+										type={showPassword ? 'text' : 'password'}
+										placeholder="MinLength(8), uppercase, lowercase, character, number"
+										className={styles.change_input}
+										value={form.oldPassword}
+										onChange={oldPasswordOnChangeHandler}
+										invalid={!form.oldPasswordIsValid && form.oldPasswordIsFocus ? 'invalid' : ''}
+										onBlur={oldPasswordOnBlurHandler}
+										required
+									/>
+									<div className={styles.eye} style={{ top: '27px' }} onClick={() => setShowPassword(!showPassword)}>
+										{showPassword ? <span className={styles.eyeSvg}><AiFillEyeInvisible /></span> :
+											<span className={styles.eyeSvg}><AiFillEye /></span>}
+									</div>
+									{form.oldPasswordIsFocus && !form.oldPasswordIsValid && (
+										<pre className={styles.invalid__input}>
+											MinLength(8), uppercase, lowercase, character, number
+										</pre>
+									)}
+								</div>
 
-								<input
-									type="password"
-									placeholder="MinLength(8), uppercase, lowercase, character, number"
-									className={styles.change_input}
-									value={form.newPassword}
-									onChange={newPasswordOnChangeHandler}
-									invalid={!form.newPasswordIsValid && form.newPasswordIsFocus ? 'invalid' : ''}
-									onBlur={newPasswordOnBlurHandler}
-									required
-								/>
-								{form.newPasswordIsFocus && !form.newPasswordIsValid && (
-									<pre className={styles.invalid__input}>
-										MinLength(8), uppercase, lowercase, character, number
-									</pre>
-								)}
+								<label htmlFor="newpass">New Password</label>
+								<div className={styles.input__cont}>
+									<input
+										type={showNewPassword ? 'text' : 'password'}
+										placeholder="MinLength(8), uppercase, lowercase, character, number"
+										className={styles.change_input}
+										value={form.newPassword}
+										onChange={newPasswordOnChangeHandler}
+										invalid={!form.newPasswordIsValid && form.newPasswordIsFocus ? 'invalid' : ''}
+										onBlur={newPasswordOnBlurHandler}
+										required
+									/>
+									<div className={styles.eye} style={{ top: '27px' }} onClick={() => setShowNewPassword(!showNewPassword)}>
+										{showNewPassword ? <span className={styles.eyeSvg}><AiFillEyeInvisible /></span> :
+											<span className={styles.eyeSvg}><AiFillEye /></span>}
+									</div>
+									{form.newPasswordIsFocus && !form.newPasswordIsValid && (
+										<pre className={styles.invalid__input}>
+											MinLength(8), uppercase, lowercase, character, number
+										</pre>
+									)}
+								</div>
 
 								<label htmlFor="confirmNewPass">Confirm New Password</label>
-								<input
-									type="password"
-									name="cofirmNewPass"
-									placeholder="MinLength(8), uppercase, lowercase, character, number"
-									className={styles.change_input}
-									value={confirmPassword}
-									onChange={(e) => {
-										setConfirmPassword(e.target.value);
-									}}
-									required
-								/>
+								<div className={styles.input__cont}>
+									<input
+										type={showConfirmPassword ? 'text' : 'password'}
+										name="cofirmNewPass"
+										placeholder="MinLength(8), uppercase, lowercase, character, number"
+										className={styles.change_input}
+										value={form.confirmPassword}
+										onChange={confirmPasswordOnChangeHandler}
+										invalid={!form.confirmPasswordIsValid && form.confirmPasswordIsFocus ? 'invalid' : ''}
+										onBlur={confirmPasswordOnBlurHandler}
+										required
+									/>
+									<div className={styles.eye} style={{ top: '27px' }} onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+										{showConfirmPassword ? <span className={styles.eyeSvg}><AiFillEyeInvisible /></span> :
+											<span className={styles.eyeSvg}><AiFillEye /></span>}
+									</div>
+									{form.confirmPasswordIsFocus && !form.confirmPasswordIsValid && (
+										<pre className={styles.invalid__input}>
+											MinLength(8), uppercase, lowercase, character, number
+										</pre>
+									)}
+								</div>
 
-								{form.newPassword === confirmPassword && form.newPassword !== '' ? <button onClick={changePassword}  >Update Changes</button> : <>
+								{form.newPassword === form.confirmPassword && form.newPassword !== '' ? <button onClick={changePassword}  >Update Changes</button> : <>
 									<button disabled style={{ opacity: 0.5 }}>Update Changes</button>
 								</>
-
 								}
 							</div>
 						</form>
