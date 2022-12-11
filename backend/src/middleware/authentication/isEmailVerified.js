7086a5cad7a0ce1c5bae5e116190f0ef88c331d1
Chima <orlandofays3@gmail.com>
2022-12-11 13:56:01 +0100

    fix email verification issue

--- backend/src/middleware/authentication/isEmailVerified.js
+++ backend/src/middleware/authentication/isEmailVerified.js
@@ -1,16 +1,12 @@
 import { AuthorizationError } from "../../lib/errors/index.js";
 
-export default () => {
-    return async (req, res, next) => {
+export default (user) => {
         try {
-            const user = req.session.user;
             if (user.email_verified === 0) {
                 throw new AuthorizationError("Kindly verify your email to continue.");
             }
-            return next(); 
         } catch (error) {
-            next(error);
+            throw new AuthorizationError(error.message)
         }
         
     }
-}
