c2bfdefb07882e2d9242a72f122eb31cd2732588
Joel Ojerinde <joelojerinde@gmail.com>
2022-12-12 10:37:43 +0100

    feat: create servers context

--- frontend_website/src/Pages/Server/ServerDashboard/ServerDashboard.jsx
+++ frontend_website/src/Pages/Server/ServerDashboard/ServerDashboard.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import ServerContext from '../../../Component/Context/ServerContext';
 import ServerInfo from '../../../Component/ServerInfo/ServerInfo';
 import SideNav from '../../../Component/SideNav/SideNav';
+import { ServerContext } from '../../../store/ServerContext';
 import { UserContext } from '../../../store/UserContext';
 import Notification from '../Notification/Notification';
 import styles from './ServerDashBoard.module.css';
 
 function ServerDashBoard() {
   const { servers } = useContext(ServerContext);
+
   const params = useParams();
   const server = servers.find((server) => server.id === params.id);
-
   const { simpleNotifications, availabilityNotifications } =
     useContext(UserContext);
 
