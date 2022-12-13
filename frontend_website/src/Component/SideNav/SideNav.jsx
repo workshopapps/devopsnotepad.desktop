41ad610adfbe7cc7dda05a0d05d744eafa9b837f
Joel Ojerinde <joelojerinde@gmail.com>
2022-12-13 14:13:59 +0100

    fix: fix delete server bug

--- frontend_website/src/Component/SideNav/SideNav.jsx
+++ frontend_website/src/Component/SideNav/SideNav.jsx
@@ -26,6 +26,7 @@ function SideNav() {
   };
 
   const deleteServerHandler = (server_id) => {
+    console.log(server_id);
     const confirmDelete = prompt(
       'Are you sure you want to delete server? Answer with a Yes or No',
     );
