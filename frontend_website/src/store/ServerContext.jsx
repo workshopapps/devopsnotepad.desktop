c5e3539c0303da5b80156dd6c2e1ccfe7b25e6a8
dread557 <dadaolawunmi09@gmail.com>
2022-12-14 11:20:52 +0100

    fix css bugs and changed userId to id

--- frontend_website/src/store/ServerContext.jsx
+++ frontend_website/src/store/ServerContext.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useCallback } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 // Creating an app wide state store using the context API
 export const ServerContext = React.createContext({
   servers: [],
-  addServers: () => {},
+  addServers: () => { },
 });
 
 // Creating a component that will provide the context.
@@ -15,9 +15,9 @@ const ServerContextProvider = (props) => {
 
   // Functions to updates states. useCallback ensures that the functions are memoized
   const addServers = useCallback(async (data) => {
-    const id = await uuidv4();
+    // const id = await uuidv4();
     const formattedServers = data.servers.map((server) => ({
-      id: id,
+      // id: id,
       ...server,
     }));
     console.log(formattedServers, 'formatted servers');
@@ -29,6 +29,8 @@ const ServerContextProvider = (props) => {
     servers,
     addServers,
   };
+
+  console.log(servers)
 
   return (
     <ServerContext.Provider value={data}>
