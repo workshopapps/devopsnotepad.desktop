8d0db160b5bcd77a2b9e1a0179fedf34f0b5a764
dread557 <dadaolawunmi09@gmail.com>
2023-01-17 17:38:38 +0100

    feat: implement design of the free trial page

---
+++ frontend_website/src/Pages/FreeTrial/Slide/Slide.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import style from './Slide.module.css'
+
+import img1 from '../../../assets/free_trial-assets/img1.png'
+import img2 from '../../../assets/free_trial-assets/img2.png'
+import img3 from '../../../assets/free_trial-assets/img3.png'
+import img4 from '../../../assets/free_trial-assets/img4.png'
+import img5 from '../../../assets/free_trial-assets/img5.png'
+
+const Slide = () => {
+
+    const { scrollYProgress } = useScroll()
+    const x = useTransform(scrollYProgress, [0, 1], [0, 100])
+
+    const data = [img1, img2, img3, img4, img5]
+    return (
+        <div className={style.slide}>
+            <h1 className={style.slideH1}><span className={style.blue}>Trusted</span> by happy Clients</h1>
+            <motion.div style={{ x }} className={style.images}>
+                {data.map((img, index) => (
+                    <img key={index} src={img} alt='' />
+                ))}
+            </motion.div>
+        </div>
+    )
+}
+
+export default Slide
