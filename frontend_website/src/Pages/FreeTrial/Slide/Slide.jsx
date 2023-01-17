35455a1d72362ab70bea51731196488e3b819699
dread557 <dadaolawunmi09@gmail.com>
2023-01-18 00:08:05 +0100

    fix: fix style bug

--- frontend_website/src/Pages/FreeTrial/Slide/Slide.jsx
+++ frontend_website/src/Pages/FreeTrial/Slide/Slide.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion, useScroll, useTransform } from 'framer-motion'
+// import { motion, useScroll, useTransform } from 'framer-motion'
 import style from './Slide.module.css'
 
 import img1 from '../../../assets/free_trial-assets/img1.png'
@@ -10,18 +10,18 @@ import img5 from '../../../assets/free_trial-assets/img5.png'
 
 const Slide = () => {
 
-    const { scrollYProgress } = useScroll()
-    const x = useTransform(scrollYProgress, [0, 1], [0, 100])
+    // const { scrollYProgress } = useScroll()
+    // const x = useTransform(scrollYProgress, [0, 1], [0, 100])
 
     const data = [img1, img2, img3, img4, img5]
     return (
         <div className={style.slide}>
             <h1 className={style.slideH1}><span className={style.blue}>Trusted</span> by happy Clients</h1>
-            <motion.div style={{ x }} className={style.images}>
+            <div className={style.images}>
                 {data.map((img, index) => (
                     <img key={index} src={img} alt='' />
                 ))}
-            </motion.div>
+            </div>
         </div>
     )
 }
