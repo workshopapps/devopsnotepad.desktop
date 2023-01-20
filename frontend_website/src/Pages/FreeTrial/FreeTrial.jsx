670cbaa051c8637e7f1bf57f4e9c0322e15b52c4
Joel Ojerinde <joelojerinde@gmail.com>
2023-01-20 17:59:26 +0100

    fix: fix style bug

--- frontend_website/src/Pages/FreeTrial/FreeTrial.jsx
+++ frontend_website/src/Pages/FreeTrial/FreeTrial.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Footer from '../../Component/Footer/Footer';
 import Navigation from '../../Component/Navigation/Navigation';
-import Faq from './FAQ/Faq';
-import style from './FreeTrial.module.css'
+import style from './FreeTrial.module.css';
 import HeroSection from './HeroSection/HeroSection';
 import Info from './Info/Info';
 import MailSection from './MailSection/MailSection';
+import Faqs from './NewFaqs/Faqs';
 import Slide from './Slide/Slide';
 import Testimonial from './Testimonial/Testimonial';
 
 const FreeTrial = () => {
-    return (
-        <>
-            {/* SEO optimazation */}
-            <Helmet>
-                <title>Free trial</title>
-                <meta name='description' content='Free trial' />
-                <link rel='canonical' href='/free-trial' />
-            </Helmet>
-            <Navigation />
-            {/* Application */}
-            <section className={style.TWrapper}>
-                <HeroSection />
-                <Info />
-                <Testimonial />
-                <Slide />
-                <MailSection />
-                <Faq />
-            </section>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      {/* SEO optimazation */}
+      <Helmet>
+        <title>Free trial</title>
+        <meta name='description' content='Free trial' />
+        <link rel='canonical' href='/free-trial' />
+      </Helmet>
+      <Navigation />
+      {/* Application */}
+      <section className={style.TWrapper}>
+        <HeroSection />
+        <Info />
+        <Testimonial />
+        <Slide />
+        <MailSection />
+        <Faqs />
+      </section>
+      <Footer />
+    </>
+  );
+};
 
-export default FreeTrial
+export default FreeTrial;
