1b200046c50352b5a9b336e11f8945def2fb0a39
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-01-22 15:53:41 +0100

    Update: conditional rendering in demo.jsx

--- frontend_website/src/Pages/Demo/Demo.jsx
+++ frontend_website/src/Pages/Demo/Demo.jsx
@@ -3,7 +3,7 @@ import DemoPage from '../DemoPage/DemoPage';
 import GetDemo from '../GetDemo/GetDemo';
 
 function Demo() {
-  const [demoView, setDemoView] = useState(false);
+  const [demoView, setDemoView] = useState(null);
   useEffect(() => {
     const signedUp = JSON.parse(
       localStorage.getItem('d5339e41-f0c2-46b9-b3bb-038c767c4ebb'),
@@ -11,19 +11,23 @@ function Demo() {
       ? JSON.parse(localStorage.getItem('d5339e41-f0c2-46b9-b3bb-038c767c4ebb'))
       : false;
 
-    signedUp ? setDemoView(true) : setDemoView(false);
+    signedUp ? setDemoView(1) : setDemoView(2);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return demoView ? (
-    <DemoPage />
+    demoView === 1 ? (
+      <DemoPage />
+    ) : (
+      <GetDemo
+        setDemoView={() => {
+          setDemoView(1);
+        }}
+      />
+    )
   ) : (
-    <GetDemo
-      setDemoView={() => {
-        setDemoView(true);
-      }}
-    />
+    <div></div>
   );
 }
 
